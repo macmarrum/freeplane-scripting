8ceed13438430123fe0098799e6550e1d772aeec
macmarrum <7197131+macmarrum@users.noreply.github.com>
2021-10-19 13:17:41 +0200

    .

---
+++ scripts/toggleShowAttribute.groovy
@@ -0,0 +1,18 @@
+import org.freeplane.features.attribute.AttributeTableLayoutModel
+import org.freeplane.features.attribute.ModelessAttributeController
+import org.freeplane.features.map.MapModel
+import org.freeplane.features.mode.Controller
+
+// methods copied from AttributeViewTypeAction.java
+def getAttributeViewType() {
+    final MapModel map = Controller.getCurrentController().getMap()
+    return ModelessAttributeController.getController().getAttributeViewType(map)
+}
+
+def setAttributeViewType(final String type) {
+    final MapModel map = Controller.getCurrentController().getMap()
+    ModelessAttributeController.getController().setAttributeViewType(map, type)
+}
+
+def attributeViewType = getAttributeViewType() == AttributeTableLayoutModel.HIDE_ALL ? AttributeTableLayoutModel.SHOW_ALL : AttributeTableLayoutModel.HIDE_ALL
+setAttributeViewType(attributeViewType)
