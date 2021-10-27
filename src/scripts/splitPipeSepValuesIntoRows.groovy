176aaff2465dd4638385f3df8ab77667c5490291
macmarrum <7197131+macmarrum@users.noreply.github.com>
2021-10-27 14:18:57 +0200

    .

---
+++ src/scripts/splitPipeSepValuesIntoRows.groovy
@@ -0,0 +1,10 @@
+// @ExecutionModes({ON_SINGLE_NODE})
+import org.freeplane.api.NodeRO
+import org.freeplane.api.Node as FPN
+c.selecteds.each { NodeRO nodeToBeSplit ->
+    FPN newlyCreatedChild = nodeToBeSplit  // the initial one is the original node
+    nodeToBeSplit.text.tokenize('|').each {
+        newlyCreatedChild = newlyCreatedChild.createChild()
+        newlyCreatedChild.text = it
+    }
+}
