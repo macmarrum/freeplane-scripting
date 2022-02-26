dbead424c8441f680e900740a67aa0a31bc25933
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-02-26 09:25:43 +0100

    fix: abort when regexPattern is null; show special character in the message

--- src/scripts/splitOnRegex.groovy
+++ src/scripts/splitOnRegex.groovy
@@ -3,11 +3,13 @@ import org.freeplane.api.Node as FPN
 
 import javax.swing.JOptionPane
 
-def regexPattern = ui.showInputDialog(c.selected.delegate, '', 'Split On Regex', JOptionPane.QUESTION_MESSAGE)
-c.selecteds.each { FPN nodeToBeSplit ->
-    FPN newlyCreatedChild = nodeToBeSplit  // the initial one is the original node
-    nodeToBeSplit.text.split(regexPattern).each { String it ->
-        newlyCreatedChild = newlyCreatedChild.createChild(it.trim())
-        newlyCreatedChild.style.name = nodeToBeSplit.style.name
+def regexPattern = ui.showInputDialog(c.selected.delegate, 'NB Special characters <([{\\^-=$!|]})?*+.>', 'Split On Regex', JOptionPane.QUESTION_MESSAGE)
+if (regexPattern !== null) {
+    c.selecteds.each { FPN nodeToBeSplit ->
+        FPN newlyCreatedChild = nodeToBeSplit  // the initial one is the original node
+        nodeToBeSplit.text.split(regexPattern).each { String it ->
+            newlyCreatedChild = newlyCreatedChild.createChild(it.trim())
+            newlyCreatedChild.style.name = nodeToBeSplit.style.name
+        }
     }
 }
