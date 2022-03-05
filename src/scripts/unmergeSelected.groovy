13ec20a936d23ad3122c427167d811edfa484584
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-05 18:53:58 +0100

    fix isLeft

--- src/scripts/unmergeSelected.groovy
+++ src/scripts/unmergeSelected.groovy
@@ -2,7 +2,7 @@
 import org.freeplane.api.Node as FPN
 
 /**
- * The opposite of merge. Split parent so that each child has its own clone
+ * The opposite of merge. Split self so that each child has its own clone
  */
 
 Collection<FPN> selecteds = c.selecteds.collect()
@@ -13,6 +13,7 @@ selecteds.each { FPN toBeSplit ->
     toBeSplit.children.eachWithIndex { child, i ->
         if (i > 0) {
             def clone = parent.appendAsCloneWithoutSubtree(toBeSplit)
+            clone.left = toBeSplit.isLeft()
             clone.moveTo(parent, ++position)
             child.moveTo(clone)
         }
@@ -20,29 +21,3 @@ selecteds.each { FPN toBeSplit ->
 }
 
 c.select(selecteds)
-
-/*
-def parent_to_children = new LinkedHashMap<FPN, ArrayList<FPN>>()
-
-selecteds.each { FPN selected ->
-    if (selected.parent && selected.parent.parent) {
-        if (parent_to_children[selected.parent] == null)
-            parent_to_children[selected.parent] = new ArrayList<FPN>()
-        parent_to_children[selected.parent] << selected
-    }
-}
-
-parent_to_children.each { e ->
-    def parent = e.key
-    def parentPosition = parent.parent.getChildPosition(parent)
-    e.value.eachWithIndex { selected, i ->
-        if (i > 0) {
-            def parentClone = parent.parent.appendAsCloneWithoutSubtree(parent)
-            parentClone.moveTo(parent.parent, ++parentPosition)
-            selected.moveTo(parentClone)
-        }
-    }
-}
-
-c.select(selecteds)
-*/
