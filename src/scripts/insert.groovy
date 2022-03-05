9898c2c9b6db7bf3d93201ac665d055565d702ed
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-05 18:54:58 +0100

    add copying of cloud

--- src/scripts/insert.groovy
+++ src/scripts/insert.groovy
@@ -1,12 +1,14 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
-
+import org.freeplane.api.Node as FPN
 def canCopyFormatToNewChild = config.getBooleanProperty("copyFormatToNewChild")
 def canCopyFormatToNewNodeIncludesIcons = config.getBooleanProperty("copyFormatToNewNodeIncludesIcons")
 def toBeSelected = new ArrayList()
-c.selecteds.each { selected ->
+c.selecteds.each { FPN selected ->
     def newNode = selected.createChild()
     if (canCopyFormatToNewChild) {
         newNode.style.name = selected.style.name
+        newNode.cloud.color = selected.cloud.color
+        newNode.cloud.shape = selected.cloud.shape
         if (canCopyFormatToNewNodeIncludesIcons)
             selected.icons.each { icon -> newNode.icons.add(icon) }
     }
