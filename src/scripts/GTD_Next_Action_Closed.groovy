b9ac9f5a399ac1875782b7961eb6253e9015743c
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-05 13:24:22 +0100

    move to submenu GTD

--- src/scripts/GTD_Next_Action_Closed.groovy
+++ src/scripts/GTD_Next_Action_Closed.groovy
@@ -1,2 +1,2 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/GTD"})
 c.selecteds.each { X.setStyleAndTimestampInAttribute('!NextAction.Closed', it) }
