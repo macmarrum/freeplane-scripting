6efc7adeaaf1ef500185348162a1d59f73ffee7d
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-06 09:50:46 +0100

    move to submenu

--- src/scripts/removeConnectorsOut.groovy
+++ src/scripts/removeConnectorsOut.groovy
@@ -1,4 +1,4 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Remove"})
 c.selecteds.each { n ->
     n.connectorsOut.each { n.removeConnector(it) }
 }
--- src/scripts/removeLink.groovy
+++ src/scripts/removeLink.groovy
@@ -1,2 +1,2 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Remove"})
 c.selecteds.each { it.link.remove() }
