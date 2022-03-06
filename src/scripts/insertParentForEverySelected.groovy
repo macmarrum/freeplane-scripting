6e43ce807b4001d676d16af122cd7e008ad214b1
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-06 09:27:16 +0100

    add copying for format and cloud; move to submenu

--- src/scripts/insertParentForEverySelected.groovy
+++ src/scripts/insertParentForEverySelected.groovy
@@ -1,15 +1,38 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
-def myPosition
-def myNewParent
-def toBeSelected = new HashSet()
-def selecteds = c.selecteds.collect()
-def positions = selecteds.collect { it.parent.getChildPosition(it) }
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Insert"})
+import org.freeplane.api.Node as FPN
 
-selecteds.eachWithIndex { self, idx ->
-    myPosition = positions[idx]
-    myNewParent = self.parent.createChild(myPosition)
-    myNewParent.setLeft(self.isLeft())
-    self.moveTo(myNewParent)
-    toBeSelected.add(myNewParent)
+/*
+ * If copyFormatToNewChild, format is copied from the parent
+ * else if copyFormatToNewSibling, format is copied from the selected node (child-to-be)
+ * Cloud is copied alongside the format
+ * No format is copied if both options are off
+ * If copyFormatToNewNodeIncludesIcons, icons are copied alongside the format
+ */
+boolean canCopyFormatToNewChild = config.getBooleanProperty("copyFormatToNewChild")
+boolean canCopyFormatToNewSibling = config.getBooleanProperty("copyFormatToNewSibling")
+boolean canCopyFormatToNewNodeIncludesIcons = config.getBooleanProperty("copyFormatToNewNodeIncludesIcons")
+FPN parent
+FPN newParent
+FPN source
+def selecteds = c.selecteds.collect()
+def positions = selecteds.collect { FPN it -> it.parent.getChildPosition(it) }
+Integer position
+def toBeSelected = new HashSet<FPN>()
+
+selecteds.eachWithIndex { FPN selected, idx ->
+    parent = selected.parent
+    position = positions[idx]
+    newParent = parent.createChild(position)
+    newParent.left = selected.left
+    selected.moveTo(newParent)
+    if (canCopyFormatToNewChild || canCopyFormatToNewSibling) {
+        source = canCopyFormatToNewChild ? parent : canCopyFormatToNewSibling ? selected : null
+        newParent.style.name = source.style.name
+        newParent.cloud.color = source.cloud.color
+        newParent.cloud.shape = source.cloud.shape
+        if (canCopyFormatToNewNodeIncludesIcons)
+            source.icons.each { icon -> newParent.icons.add(icon) }
+    }
+    toBeSelected.add(newParent)
 }
 c.select(toBeSelected)
