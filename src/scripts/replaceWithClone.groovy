8dfd37448c455716f474633d30732a59d7674b5d
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-15 22:26:26 +0100

    code clean-up

--- src/scripts/replaceWithClone.groovy
+++ src/scripts/replaceWithClone.groovy
@@ -1,27 +1,36 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
 
 import groovy.xml.XmlParser
-import org.freeplane.api.Node as FPN
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+import org.freeplane.core.util.MenuUtils
 import org.freeplane.features.map.clipboard.MapClipboardController
 import org.freeplane.features.map.clipboard.MindMapNodesSelection
+import org.freeplane.features.map.mindmapmode.clipboard.MMapClipboardController
+import org.freeplane.plugin.script.proxy.ScriptUtils
 
+import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Transferable
 
-def toBeSelected = new LinkedList<FPN>()
-def copiedNodes = getNodesFromClipboardXml(getXml(MapClipboardController.controller.clipboardContents))
-List<FPN> selecteds = c.selecteds.collect()
+def toBeSelected = new LinkedList<Node>()
+final Transferable t = ((MMapClipboardController) MapClipboardController.controller).clipboardContents
+def copiedNodes = getNodesFromClipboardXml(getXml(t))
+Controller c = ScriptUtils.c()
+List<Node> selecteds = c.selecteds.collect()
 if (copiedNodes.size() != selecteds.size() && !(copiedNodes.size() == 1 && selecteds.size() > 1)) {
     c.statusInfo = "convertToClone: got ${copiedNodes.size()} nodes from clipboard and ${selecteds.size()} target nodes are selected -- expected the same count or 1 to many"
 } else {
     def originalChildren
-    FPN source
+    Node source
     int position
-    FPN clone
+    Node clone
+    MenuUtils menuUtils = new MenuUtils()
+    final copySingleAction = ['CopySingleAction']
     selecteds.eachWithIndex { target, i ->
         originalChildren = target.parent.children.collect()
         source = copiedNodes.size() == 1 ? copiedNodes[0] : copiedNodes[i]
         c.select(source)
-        menuUtils.executeMenuItems(['CopySingleAction'])
+        menuUtils.executeMenuItems(copySingleAction)
         position = target.parent.getChildPosition(target)
         target.parent.pasteAsClone()
         clone = target.parent.children[-1]
@@ -33,12 +42,12 @@ if (copiedNodes.size() != selecteds.size() && !(copiedNodes.size() == 1 && selec
 }
 c.select(toBeSelected)
 
-private List<FPN> getNodesFromClipboardXml(String xml) {
+private List<Node> getNodesFromClipboardXml(String xml) {
     try {
         def parser = new XmlParser()
         return xml.split(MapClipboardController.NODESEPARATOR).collect { String it ->
             def xmlRootNode = parser.parseText(it)
-            N(xmlRootNode.@ID)
+            ScriptUtils.node().mindMap.node(xmlRootNode.@ID)
         }
     } catch (ignored) {
     }
@@ -50,6 +59,12 @@ private static String getXml(Transferable t) {
         try {
             return t.getTransferData(MindMapNodesSelection.mindMapNodesFlavor).toString()
         } catch (ignored) {
+        }
+    } else {
+        println(":: tranferable is missing ${MindMapNodesSelection.mindMapNodesFlavor}")
+        println(":: available transferables:")
+        t.getTransferDataFlavors().each { DataFlavor dataFlavor ->
+            println(dataFlavor)
         }
     }
     return null
