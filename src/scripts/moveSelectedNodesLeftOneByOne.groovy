beda13ece94746602303def096c7906c955a5948
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-16 09:20:13 +0100

    refactor

--- src/scripts/moveSelectedNodesLeftOneByOne.groovy
+++ src/scripts/moveSelectedNodesLeftOneByOne.groovy
@@ -1,7 +1,12 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Move"})
-def selecteds = c.selecteds.collect()
+import org.freeplane.core.util.MenuUtils
+import org.freeplane.plugin.script.proxy.ScriptUtils
+
+final changeNodeLevelLeftsAction = ['ChangeNodeLevelLeftsAction']
+final c = ScriptUtils.c()
+final selecteds = c.selecteds.collect()
 selecteds.each {
     c.select(it)
-    menuUtils.executeMenuItems(['ChangeNodeLevelLeftsAction'])
+    MenuUtils.executeMenuItems(changeNodeLevelLeftsAction)
 }
 c.select(selecteds)
