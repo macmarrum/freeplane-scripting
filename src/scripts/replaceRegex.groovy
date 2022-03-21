236daa529714da9d7cfbd6acd34cce5a7a39662a
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-03-21 09:03:39 +0100

    fix getting replacement, add storing input

--- src/scripts/replaceRegex.groovy
+++ src/scripts/replaceRegex.groovy
@@ -1,15 +1,36 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
 import org.freeplane.api.Node as FPN
+import org.freeplane.core.resources.ResourceController
+import org.freeplane.core.ui.components.UITools
+import org.freeplane.features.map.NodeModel
+import org.freeplane.plugin.script.proxy.ScriptUtils
 
-import javax.swing.JOptionPane
-
-def regexPattern = ui.showInputDialog(c.selected.delegate, 'NB Special characters <([{\\^-=$!|]})?*+.>', 'Replace Regex', JOptionPane.QUESTION_MESSAGE)
-if (regexPattern !== null) {
-    def lst = regexPattern.tokenize(regexPattern[0])
+final String default_browser_command_windows_nt = "default_browser_command_windows_nt"
+final resourceController = ResourceController.getResourceController()
+final c = ScriptUtils.c()
+final NodeModel nodeModel = c.selected.delegate
+final String initialValue = resourceController.getProperty(default_browser_command_windows_nt)
+String input = UITools.showInputDialog(nodeModel, 'Replace Regex\nE.g. /pattern/replacement/g\nNB Special characters <([{\\^-=$!|]})?*+.>', initialValue)
+if (input !== null) {
+    resourceController.setProperty(default_browser_command_windows_nt, input)
+    final token = input[0]
+    final tokenCount = input.count(token)
+    if (tokenCount != 3) {
+        c.statusInfo = "Replace Regex: token count is $tokenCount - expected 3"
+        return
+    }
+    final lst = input.tokenize(token)
+    final String pattern = lst[0]
+    final boolean isGlobal = input.endsWith("${token}g")
+    String replacement
+    if ((isGlobal && input.endsWith("$token${token}g")) || input.endsWith("$token$token"))
+        replacement = ''
+    else
+        replacement = lst[1]
     c.selecteds.each { FPN n ->
-        if (lst.size() == 3 && lst[2] == 'g')
-            n.text = n.text.replaceAll(lst[0], lst[1])
+        if (isGlobal)
+            n.text = n.text.replaceAll(pattern, replacement)
         else
-            n.text = n.text.replaceFirst(lst[0], lst[1])
+            n.text = n.text.replaceFirst(pattern, replacement)
     }
 }
