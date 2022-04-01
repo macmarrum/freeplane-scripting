2247a2498bfd09b9816957af2a1147a903bae41a
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-04-01 22:24:35 +0200

    change centering from node to window

--- src/scripts/gotoAlias.groovy
+++ src/scripts/gotoAlias.groovy
@@ -2,25 +2,16 @@
 import org.freeplane.api.Node
 import org.freeplane.features.explorer.NodeAlias
 import org.freeplane.features.explorer.NodeAliases
-import org.freeplane.features.map.NodeModel
 import org.freeplane.features.mode.Controller
-import org.freeplane.features.ui.IMapViewManager
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
-import javax.swing.*
-import java.awt.*
+import javax.swing.JOptionPane
 
 final c = ScriptUtils.c()
-final node = ScriptUtils.node()
-final NodeModel nodeModel = node.delegate
-
 final Controller controller = Controller.getCurrentController()
-final IMapViewManager viewController = controller.getMapViewManager()
-final Component parentComponent = viewController.getComponent(nodeModel)
-
 final Collection<NodeAlias> nodeAliases = NodeAliases.of(controller.map).aliases()
 final String[] aliases = nodeAliases.collect { it.value }.sort().toArray()
-String input = JOptionPane.showInputDialog(parentComponent, null, "Go to alias",
+String input = JOptionPane.showInputDialog(null, null, "Go to alias",
         JOptionPane.QUESTION_MESSAGE, null, aliases, null)
 if (input == null || input == '') {
     c.statusInfo = 'input is null or blank'
