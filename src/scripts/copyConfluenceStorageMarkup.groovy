0c9f4be05405404fc37fd3106e7eaaf2bdbff0aa
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-04-04 13:09:00 +0200

    fix executing args with spaces, improve vim modeline, add getting the editor command from config

--- src/scripts/copyConfluenceStorageMarkup.groovy
+++ src/scripts/copyConfluenceStorageMarkup.groovy
@@ -1,25 +1,39 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
 import ConfluenceStorage
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
 import org.freeplane.core.util.TextUtils
+import org.freeplane.plugin.script.FreeplaneScriptBaseClass
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
-def node = ScriptUtils.node()
-def c = ScriptUtils.c()
+Node node = ScriptUtils.node()
+Controller c = ScriptUtils.c()
 if (node.style.name in ConfluenceStorage.style) { // ['cStorageMarkupRoot', 'cStorageMarkupMaker'])
     String markup = ConfluenceStorage.makeMarkup(node)
     TextUtils.copyToClipboard(markup)
-    def mmFile = node.mindMap.file
-    def xmlFileBasename = mmFile.name.replaceFirst(/\.mm$/, '.cStorage')
-    def xmlFile = new File(mmFile.parent, xmlFileBasename)
-    try {
-        xmlFile.withWriter('UTF-8') {
-            it << '<!-- vim: ft=xml \n-->\n'
-            it << markup
-        }
-        "gvim $xmlFile.path".execute()
-    } catch (RuntimeException e) {
-        c.statusInfo = e.message
-    }
+    openInEditorIfDefined(c, node, markup)
 } else {
     c.statusInfo = "cannot copy ConfluenceStorage Markup because the node style is not in ${ConfluenceStorage.style*.value}"
+}
+
+/*
+ * Uses default_browser_command_mac to define the editor -- must be in PATH
+ */
+static void openInEditorIfDefined(Controller c, Node node, String markup) {
+    def config = new FreeplaneScriptBaseClass.ConfigProperties()
+    def default_browser_command_mac = config.getProperty('default_browser_command_mac')
+    if (default_browser_command_mac && default_browser_command_mac != 'open') {
+        File mmFile = node.mindMap.file
+        def xmlFileBasename = mmFile.name.replaceFirst(/\.mm$/, '.cStorage')
+        def xmlFile = new File(mmFile.parent, xmlFileBasename)
+        try {
+            xmlFile.withWriter('UTF-8') {
+                it << '<!-- vim: set ft=xml: -->\n'
+                it << markup
+            }
+            [default_browser_command_mac, xmlFile.path].execute()
+        } catch (RuntimeException e) {
+            c.statusInfo = e.message
+        }
+    }
 }
