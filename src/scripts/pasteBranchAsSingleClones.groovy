fba6209389f6e891f121b95db5be0ae2df4521f0
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-05-10 22:56:16 +0200

    ALt+Shift+D for pasteBranchAsSingleClones

---
+++ src/scripts/pasteBranchAsSingleClones.groovy
@@ -0,0 +1,58 @@
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Paste"})
+/*
+ * Extends https://www.freeplane.org/wiki/index.php/Scripts_collection#Access_nodes_from_clipboard
+ */
+
+import groovy.xml.XmlParser
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+import org.freeplane.features.map.clipboard.MapClipboardController
+import org.freeplane.features.map.clipboard.MindMapNodesSelection
+import org.freeplane.features.map.mindmapmode.clipboard.MMapClipboardController
+import org.freeplane.plugin.script.proxy.ScriptUtils
+
+import java.awt.datatransfer.Transferable
+
+Controller c = ScriptUtils.c()
+final Transferable t = ((MMapClipboardController) MapClipboardController.controller).clipboardContents
+def copiedNodes = getNodesFromClipboard(getXml(t))
+def toBeSelected = new LinkedList<Node>()
+copiedNodes.each { Node source ->
+    c.selecteds.each { Node targetParent ->
+        pasteAsSingleCloneRecursivelyAndAddToBeSelected(source, targetParent, toBeSelected)
+    }
+}
+c.select(toBeSelected)
+
+private static List<Node> getNodesFromClipboard(String xml) {
+    Node node = ScriptUtils.node()
+    try {
+        def parser = new XmlParser()
+        return xml.split(MapClipboardController.NODESEPARATOR).collect { String it ->
+            def xmlRootNode = parser.parseText(it)
+            node.mindMap.node(xmlRootNode.@ID)
+        }
+    }
+    catch (ignored) {
+    }
+    return []
+}
+
+private static String getXml(Transferable t) {
+    if (t.isDataFlavorSupported(MindMapNodesSelection.mindMapNodesFlavor)) {
+        try {
+            return t.getTransferData(MindMapNodesSelection.mindMapNodesFlavor).toString()
+        }
+        catch (ignored) {
+        }
+    }
+    return null
+}
+
+static void pasteAsSingleCloneRecursivelyAndAddToBeSelected(Node source, Node targetParent, List<Node> toBeSelected) {
+    Node target = targetParent.appendAsCloneWithoutSubtree(source)
+    toBeSelected.add(target)
+    source.children.eachWithIndex { Node sourceChild, int i ->
+        pasteAsSingleCloneRecursivelyAndAddToBeSelected(sourceChild, target, toBeSelected)
+    }
+}
