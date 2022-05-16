2310e4a7a2d540b3ddc7b7536e6c95c6333f126b
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-05-16 14:15:07 +0200

    new filters

---
+++ src/scripts/filterSelectedAndSameTextNodes.groovy
@@ -0,0 +1,8 @@
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac2"})
+import org.freeplane.api.Node
+import org.freeplane.features.filter.FilterController
+
+def filterController = FilterController.currentFilterController
+def showAncestors = true
+def showDescendants = filterController.showDescendants.selected
+node.mindMap.filter(showAncestors, showDescendants, { c.selecteds.any { Node selected -> it.text == selected.text } })
