3b514ab5e2b758634316ba71ad046b252ace37c6
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-06-07 23:34:43 +0200

    fix: add individual format and conditional styles

--- src/scripts/pasteContents.groovy
+++ src/scripts/pasteContents.groovy
@@ -1,12 +1,17 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Paste"})
 
+
 import groovy.xml.XmlParser
-import org.freeplane.api.Controller
 import org.freeplane.api.Node
+import org.freeplane.features.icon.mindmapmode.MIconController
+import org.freeplane.features.map.NodeModel
 import org.freeplane.features.map.clipboard.MapClipboardController
 import org.freeplane.features.map.clipboard.MindMapNodesSelection
 import org.freeplane.features.map.mindmapmode.clipboard.MMapClipboardController
-import org.freeplane.plugin.script.proxy.ScriptUtils
+import org.freeplane.features.mode.Controller
+import org.freeplane.features.mode.ModeController
+import org.freeplane.features.styles.ConditionalStyleModel
+import org.freeplane.features.styles.LogicalStyleKeys
 
 import java.awt.datatransfer.Transferable
 
@@ -16,18 +21,19 @@ final Transferable t = ((MMapClipboardController) MapClipboardController.control
 def copiedNodes = getNodesFromClipboardXml(getXml(t))
 if (copiedNodes.size() == 1) {
     Node source = copiedNodes[0]
+    NodeModel sourceModel = source.delegate
     c.selecteds.each { Node target ->
+        NodeModel targetModel = target.delegate
         target.text = source.text
-        target.icons.clear()
-        target.icons.addAll(source.icons)
-        target.style.name = source.style.name
         target.detailsText = source.detailsText
         target.noteText = source.noteText
         target.attributes.clear()
-        source.attributes.each {entry ->
+        //TODO attribute key can be repeated, fix copying such attributes
+        source.attributes.each { entry ->
             target[entry.key] = entry.value
         }
-        //TODO node conditionals
+        copyFormatAndIconsBetween(sourceModel, targetModel)
+        copyNodeConditionalStylesBetween(sourceModel, targetModel)
     }
 } else {
     c.statusInfo = "pasteValuesIntoExistingNode: got ${copiedNodes.size()} nodes from clipboard -- expected 1"
@@ -55,4 +61,28 @@ private static String getXml(Transferable t) {
         }
     }
     return null
+}
+
+// based on org.freeplane.features.styles.mindmapmode.ManageNodeConditionalStylesAction.getConditionalStyleModel
+private static copyNodeConditionalStylesBetween(NodeModel sourceModel, NodeModel targetModel) {
+    targetModel.removeExtension(ConditionalStyleModel.class)
+    ConditionalStyleModel sourceCondiStyleModel = sourceModel.getExtension(ConditionalStyleModel.class)
+    if (sourceCondiStyleModel != null) {
+        ConditionalStyleModel targetCondiStyleModel = new ConditionalStyleModel()
+        targetModel.addExtension(targetCondiStyleModel)
+        sourceCondiStyleModel.styles.each { targetCondiStyleModel.styles << new ConditionalStyleModel.Item(it) }
+    }
+}
+
+// from org.freeplane.features.nodestyle.mindmapmode.PasteFormat
+static copyFormatAndIconsBetween(NodeModel source, NodeModel target) {
+    final ModeController modeController = Controller.getCurrentModeController()
+    modeController.undoableRemoveExtensions(LogicalStyleKeys.LOGICAL_STYLE, target, target)
+    modeController.undoableCopyExtensions(LogicalStyleKeys.LOGICAL_STYLE, source, target)
+    modeController.undoableRemoveExtensions(LogicalStyleKeys.NODE_STYLE, target, target)
+    modeController.undoableCopyExtensions(LogicalStyleKeys.NODE_STYLE, source, target)
+    //if(ResourceController.getResourceController().getBooleanProperty("copyFormatToNewNodeIncludesIcons")) {
+    modeController.undoableRemoveExtensions(MIconController.Keys.ICONS, target, target)
+    modeController.undoableCopyExtensions(MIconController.Keys.ICONS, source, target)
+    //}
 }
