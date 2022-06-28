1d4ce9a591534fb6015c313a800c3d69a226cb79
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-06-28 16:05:44 +0200

    add Journal style application

--- src/scripts/newJournalNode.groovy
+++ src/scripts/newJournalNode.groovy
@@ -1,6 +1,9 @@
+import org.freeplane.features.styles.MapStyleModel
+
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac2"})
 
 final journalRootStyleName = 'Journal root'
+final journalNodeStyleName = 'Journal'
 def customDateFormatForThisScriptOnly = 'yyyy-mm-dd,E'
 def defaultDateFormatAsSetInPreferences = config.getProperty('date_format')
 def defaultDateTimeFormatAsSetInPreferences = config.getProperty('datetime_format')
@@ -8,4 +11,6 @@ def now = new Date()
 def journalRoot =  c.findAll().find { it.style.name == journalRootStyleName } ?: node.mindMap.root
 def journalDate = format(now, defaultDateFormatAsSetInPreferences)
 def journalNode = journalRoot.findAll().find { it.text == journalDate.toString() } ?: journalRoot.createChild(journalDate)
+if (MapStyleModel.getExtension(node.delegate.map).styles.find { it.toString() == journalNodeStyleName })
+    journalNode.style.name = journalNodeStyleName
 c.select(journalNode)
