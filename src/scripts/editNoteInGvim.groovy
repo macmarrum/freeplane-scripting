f47c87317cdb5e727779a3daf8c865a1ee9f5ae3
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-06-28 19:41:27 +0200

    add commented notepad++

--- src/scripts/editNoteInGvim.groovy
+++ src/scripts/editNoteInGvim.groovy
@@ -3,6 +3,7 @@ def tempFile = File.createTempFile(node.id + '~', '.tmp')
 String text = node.note ?: ''
 tempFile.setText(text, 'UTF-8')
 def editorProcess = ['gvim', '--nofork', tempFile].execute()
+// def editorProcess = ['C:/Program Files/Notepad++/notepad++.exe', '-nosession', '-notabbar', '-multiInst', tempFile].execute()
 editorProcess.waitFor()
 node.note = tempFile.getText('UTF-8') ?: null
 tempFile.delete()
