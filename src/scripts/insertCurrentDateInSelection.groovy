3fe0f694a742e432e24631dca3e2b7a5d8e997ef
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-06-28 14:48:56 +0200

    remove comments

--- src/scripts/insertCurrentDateInSelection.groovy
+++ src/scripts/insertCurrentDateInSelection.groovy
@@ -1,9 +1,6 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac2"})
-//import org.freeplane.features.format.FormatController
 
 def customDateFormatForThisScriptOnly = 'yyyy-mm-dd,E'
-//def defaultDateFormatAsSetInPreferences = FormatController.controller.defaultDateFormat.toPattern()
-//def defaultDateTimeFormatAsSetInPreferences = FormatController.controller.defaultDateTimeFormat.toPattern()
 def defaultDateFormatAsSetInPreferences = config.getProperty('date_format')
 def defaultDateTimeFormatAsSetInPreferences = config.getProperty('datetime_format')
 def now = new Date()
