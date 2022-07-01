a41e12c4babe57bafb413c7e43619eb767df7f2c
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-07-01 21:15:01 +0200

    add a backlink to the source node

--- src/scripts/newNodeInInboxOfGtdMapWithBacklink.groovy
+++ src/scripts/newNodeInInboxOfGtdMapWithBacklink.groovy
@@ -4,8 +4,8 @@ import javax.swing.JOptionPane
 final mindMapName = 'GTD'
 final inboxStyleName = 'Inbox'
 
-final mindMap = c.openMindMaps.find { it.name == mindMapName }
-if (mindMap) {
+def gtdMindmap = c.openMindMaps.find { it.name == mindMapName }
+if (gtdMindmap) {
     def title = 'Capture to GTD Inbox'
     def text = ''
     def capturedText = ui.showInputDialog(node.delegate, text, title, JOptionPane.QUESTION_MESSAGE)
@@ -14,9 +14,17 @@ if (mindMap) {
 //    def customDateFormatForThisScriptOnly = 'yyyy-mm-dd,E'
 //    def defaultDateFormatAsSetInPreferences = config.getProperty('date_format')
     def defaultDateTimeFormatAsSetInPreferences = config.getProperty('datetime_format')
-    def inboxNode = mindMap.root.findAll().find { it.style.name == inboxStyleName }
+    def inboxNode = gtdMindmap.root.findAll().find { it.style.name == inboxStyleName }
     def newNode = inboxNode.createChild(capturedText)
     newNode.details = format(new Date(), defaultDateTimeFormatAsSetInPreferences).toString()
+    String sourceMindmapPath
+    if (gtdMindmap == node.mindMap)
+        sourceMindmapPath = ''
+    else if (config.getProperty('links') == 'relative')
+        sourceMindmapPath = gtdMindmap.file.parentFile.relativePath(node.mindMap.file)
+    else
+        sourceMindmapPath = node.mindMap.file.path
+    newNode.link.text = "${sourceMindmapPath}#${node.id}"
 } else {
     ui.showMessage("No such mindmap is open: $mindMapName", JOptionPane.ERROR_MESSAGE)
 }
