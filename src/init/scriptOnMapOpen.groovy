7fe5a84a5c5cb7d4d28c268f05598efc96424046
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-09-24 23:24:54 +0200

    remove Flag; refactor

--- src/init/scriptOnMapOpen.groovy
+++ src/init/scriptOnMapOpen.groovy
@@ -1,42 +1,28 @@
 package io.github.macmarrum.freeplane
 
 import org.freeplane.api.MindMap
-import org.freeplane.core.extension.IExtension
 import org.freeplane.core.util.LogUtils
 import org.freeplane.features.attribute.Attribute
 import org.freeplane.features.attribute.NodeAttributeTableModel
 import org.freeplane.features.map.IMapLifeCycleListener
 import org.freeplane.features.map.MapModel
-import org.freeplane.features.map.NodeModel
 import org.freeplane.features.mode.Controller
 import org.freeplane.plugin.script.ScriptingEngine
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
-class MapLifeCycleListener implements IMapLifeCycleListener {
-    static class ScriptOnMapOpenFlag implements IExtension {
-        static IExtension getExtensionOf(NodeModel nodeModel) {
-            def extensions = nodeModel.sharedExtensions.values()
-            return extensions.find { it.class.name == this.name }
-        }
-    }
-
+class MapOpenListener implements IMapLifeCycleListener {
     static final SCRIPT_ON_MAP_OPEN_ATTR_NAME = 'scriptOnMapOpen'
 
-    static executeScriptOnMapOpen(MapModel mapModel) {
-        NodeModel rootNodeModel = mapModel.rootNode
-        if (!ScriptOnMapOpenFlag.getExtensionOf(rootNodeModel)) {
-            rootNodeModel.addExtension(new ScriptOnMapOpenFlag())
-            String script
-            def attributeTable = rootNodeModel.getExtension(NodeAttributeTableModel.class)
-            if (attributeTable) {
-                attributeTable.attributes.eachWithIndex { Attribute attr, int i ->
-                    if (attr.name.startsWithIgnoreCase(SCRIPT_ON_MAP_OPEN_ATTR_NAME)) {
-                        script = attr.value
-                        if (script) {
-                            LogUtils.info("   executing ${attr.name} (#$i) for ${mapModel?.file?.name ?: mapModel.rootNode.text}")
-                            ScriptingEngine.executeScript(rootNodeModel, script)
-                        }
-                    }
+    static executeScriptOnMapOpen(MapModel map) {
+        def root = map.rootNode
+        String script
+        def attributeTable = root.getExtension(NodeAttributeTableModel.class)
+        attributeTable?.attributes?.eachWithIndex { Attribute attr, int i ->
+            if (attr.name.startsWithIgnoreCase(SCRIPT_ON_MAP_OPEN_ATTR_NAME)) {
+                script = attr.value
+                if (script) {
+                    LogUtils.info("   executing ${attr.name} (#$i) in ${map?.file?.name ?: map.rootNode.text}")
+                    ScriptingEngine.executeScript(root, script)
                 }
             }
         }
@@ -49,8 +35,8 @@ class MapLifeCycleListener implements IMapLifeCycleListener {
 }
 
 
-Controller.currentModeController.controller.addMapLifeCycleListener(new MapLifeCycleListener())
-
 ScriptUtils.c().openMindMaps.each { MindMap mindMap ->
-    MapLifeCycleListener.executeScriptOnMapOpen(mindMap.delegate)
+    MapOpenListener.executeScriptOnMapOpen(mindMap.delegate)
 }
+
+Controller.currentModeController.controller.addMapLifeCycleListener(new MapOpenListener())
