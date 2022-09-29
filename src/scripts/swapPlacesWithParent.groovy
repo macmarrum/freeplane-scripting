810f80d7136c933dafeaab45bfcf11003eedd23c
macmarrum <7197131+macmarrum@users.noreply.github.com>
2022-09-29 17:09:38 +0200

    add missing declarations; refactor

--- src/scripts/swapPlacesWithParent.groovy
+++ src/scripts/swapPlacesWithParent.groovy
@@ -1,53 +1,62 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
-def kids
-def self
-def par
+import org.freeplane.api.Node as FN
+import org.freeplane.plugin.script.proxy.ScriptUtils
+
+List<FN> children
+FN self
+FN par
+FN temp
 int myPos
 int parPos
-def toBeSelected = new ArrayList()
+def c = ScriptUtils.c()
 def selecteds = c.selecteds.collect()  // a clone
+def toBeSelected = new ArrayList<FN>(selecteds.size())
 for (n in selecteds) {
-	// no swapping with root
-	if (n.parent.isRoot()) continue
+    // no swapping with root
+    if (n.parent.isRoot())
+        continue
 
-	// no swaping with a hidden parent, e.g. a SummaryNode
-	// NB. a summary node is made of:
-	//     1. FirstGroupNode (hidden) indicating the start point
-	//     2. SummaryNode (hidden) indicating the end point
-	//		 3. SummaryNode's child, with the actual text
-	//  Freeplane displays an accolade symbol between 1 and 2, with 3 at its confluence point
-	if (!n.parent.visible) continue
+    // no swapping with a hidden parent, e.g. a SummaryNode
+    // NB. a summary node is made of:
+    //     1. FirstGroupNode (hidden) indicating the start point
+    //     2. SummaryNode (hidden) indicating the end point
+    //		 3. SummaryNode's child, with the actual text
+    //  Freeplane displays an accolade symbol between 1 and 2, with 3 at its confluence point
+    if (!n.parent.visible)
+        continue
 
-	// at the end, this will be selected
-	toBeSelected.add(n.parent)
+    // remember the objects, because they will move and their relative names won't work
+    self = n
+    par = n.parent
 
-	// remember the objects, because they will move and their relative names won't work
-	self = n
-	par = n.parent
+	toBeSelected.add(par) // at the end, this will be selected
 
-	if (self.children.size() > 0) kids = self.children
-	myPos = par.getChildPosition(self)
+    myPos = par.getChildPosition(self)
 	parPos = par.parent.getChildPosition(par)
 
-	// create temp node in my position
-	if (kids) temp = par.createChild(myPos)
+	children = self.children.size() > 0 ? self.children : null
+	if (children) {
+        temp = par.createChild(myPos) // create temp node at my position
+        children.each {
+            it.moveTo(temp)
+        }
+    } else {
+		temp = null
+    }
 
-	// move children to temp
-	if (kids) kids.each{ it.moveTo(temp) }
+    self.moveTo(par.parent, parPos) // move to grandparent
 
-	// move to grandparent
-	self.moveTo(par.parent, parPos)
+    par.children.each { // move siblings to myself
+        it.moveTo(self)
+    }
 
-	// move siblings to myself
-	par.children.each{ it.moveTo(self) }
+    par.moveTo(self, myPos) // move par to my initial position
 
-	// move par to my initial position
-	par.moveTo(self, myPos)
-
-	// move kids to par
-	if (kids) kids.each{ it.moveTo(par) }
-
-	// remove temp
-	if (kids) temp.delete()
+    if (children) {
+        children.each {
+            it.moveTo(par)
+        }
+        temp.delete()
+    }
 }
 c.select(toBeSelected)
