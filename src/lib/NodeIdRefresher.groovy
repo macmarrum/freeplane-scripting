26be2c31d73b70057e70e6ba7983c9d9ef8604f7
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-03-14 22:25:49 +0100

    store freshIdToOriginal as JSON add restoreAll and getLatestSavepoint

--- src/lib/NodeIdRefresher.groovy
+++ src/lib/NodeIdRefresher.groovy
@@ -1,9 +1,12 @@
+import groovy.json.JsonSlurper
 import org.freeplane.api.Node as FN
 
 class NodeIdRefresher {
+    public static final ATTR_PREFIX = 'freshIdToOriginal_'
 
-    static refresh(FN root) {
-        def mm = root.mindMap
+    static refreshAll(FN node) {
+        def mm = node.mindMap
+        def root = mm.root
         def nodeIdToFreshId = new LinkedHashMap<String, String>()
         String nodeId
         String freshId
@@ -19,11 +22,34 @@ class NodeIdRefresher {
             }
             it.delegate.setID(freshId)
         }
-        def sb = new StringBuilder('[\n')
-        nodeIdToFreshId.each { k, v -> sb << "$v: '$k',\n" }
-        sb << ']'
+        def sb = new StringBuilder('{\n')
+        nodeIdToFreshId.each { k, v -> sb << "\"$v\": \"$k\",\n" }
+        sb << '}'
 
         def now = (new Date()).format('yyyy-MM-dd_HH:mm:ss')
-        root["freshIdToOriginal_${now}"] = sb
+        root["${ATTR_PREFIX}${now}"] = sb
+    }
+
+    static Map.Entry<String, Object> getLatestSavepoint(FN root) {
+        def savepoints = root.attributes.findAll { it.key.startsWith(ATTR_PREFIX) }
+        if (savepoints)
+            return savepoints.sort().last()
+    }
+
+    static restoreAll(FN node) {
+        def root = node.mindMap.root
+        def savepoint = getLatestSavepoint(root)
+        if (savepoint) {
+            def hashmapAsJson = savepoint.value as String
+            def freshIdToOriginal = new JsonSlurper().parseText(hashmapAsJson)
+            String originalId
+            root.findAll().each {
+                originalId = freshIdToOriginal.get(it.id)
+                if (originalId)
+                    it.delegate.setID(originalId)
+            }
+            root['restored_' + savepoint.key] = savepoint.value
+            root.attributes.removeAll(savepoint.key)
+        }
     }
 }
