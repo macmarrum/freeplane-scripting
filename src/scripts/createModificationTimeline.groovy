2d284f2ee0097bb60761891494f0223a4ada18b7
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-04-24 13:30:46 +0200

    new

---
+++ src/scripts/createModificationTimeline.groovy
@@ -0,0 +1,44 @@
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac2"})
+
+
+import org.freeplane.core.ui.components.UITools
+import org.freeplane.plugin.script.proxy.ScriptUtils
+import org.freeplane.api.Node as FN
+
+// date format should be sortable, if the nodes on the timeline are to be sorted
+def DATE_FMT = 'yyyy-MM-dd'
+
+def mindMap = ScriptUtils.node().mindMap
+
+FN timelineNode
+timelineNode = mindMap.root.children.find { it.text == 'Timeline' }
+if (timelineNode) {
+    ScriptUtils.c().select(timelineNode)
+    UITools.showMessage('Timeline node already exists\nDelete it or rename it first', 2)
+} else {
+    timelineNode = mindMap.root.createChild('Timeline')
+    def dateToNode = new LinkedHashMap<String, FN>()
+    for (FN n in mindMap.root.findAll()) {
+        if (n.root || n == timelineNode)
+            continue
+        def dateStr = n.lastModifiedAt.format(DATE_FMT)
+        if (!dateToNode.containsKey(dateStr)) {
+            def dateNode = timelineNode.createChild(dateStr)
+            dateToNode[dateStr] = dateNode
+            dateNode['script1'] = """\
+            Date.metaClass.nextDay = {
+               use(groovy.time.TimeCategory) {
+                  delegate + 1.day
+               }
+            }
+            def date = Date.parse('${DATE_FMT}', '${dateStr}')
+            def nextDay = date.nextDay()
+            def shouldShowAncestors = true
+            def shouldShowDescendants = false
+            node.mindMap.filter(shouldShowAncestors, shouldShowDescendants, { it == node || (it.lastModifiedAt >= date && it.lastModifiedAt < nextDay) })
+            menuUtils.executeMenuItems(['SelectFilteredNodesAction'])
+            """.stripIndent()
+        }
+    }
+    timelineNode.sortChildrenBy { it.text }
+}
