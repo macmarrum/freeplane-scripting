fcdf9f85648f57ee5fb05f202f0b59b2a3513a75
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-06 18:01:40 +0200

    new scripts

---
+++ src/scripts/removeFormattingInCore.groovy
@@ -0,0 +1,3 @@
+// @ExecutionModes({ON_SELECTED_NODE="/menu_bar/Mac1/Remove"})
+// Note: getPlainText() uses HtmlUtils.htmlToPlain(), which first checks whether the text is html
+node.text = node.plainText
---
+++ src/scripts/removeFormattingInDetails.groovy
@@ -0,0 +1,2 @@
+// @ExecutionModes({ON_SELECTED_NODE="/menu_bar/Mac1/Remove"})
+node.details = org.freeplane.core.util.HtmlUtils.htmlToPlain(node.detailsText)
