b0463f2b4dd51e124461024086c4fbe38e3ec116
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-15 08:13:47 +0200

    rename

--- src/scripts/insertChildForSelected.groovy
+++ src/scripts/insertChildForSelected.groovy
@@ -1,13 +1,13 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Insert"})
-import org.freeplane.api.Node as FPN
+import org.freeplane.api.Node as FN
 
 /*
- * Cloud is copied alongside the format (part of FormatCopy/FormatPaste)
- * If copyFormatToNewNodeIncludesIcons, icons are copied alongside the format (part of FormatCopy/FormatPaste)
+ * Cloud is copied alongside node's format (part of FormatCopy/FormatPaste)
+ * If copyFormatToNewNodeIncludesIcons, icons are copied alongside node's format (part of FormatCopy/FormatPaste)
  */
 def canCopyFormatToNewChild = config.getBooleanProperty("copyFormatToNewChild")
-def toBeSelected = new LinkedList<FPN>()
-c.selecteds.collect().each { FPN selected ->
+def toBeSelected = new LinkedList<FN>()
+c.selecteds.collect().each { FN selected ->
     def newNode = selected.createChild()
     if (canCopyFormatToNewChild) {
         c.select(selected)
