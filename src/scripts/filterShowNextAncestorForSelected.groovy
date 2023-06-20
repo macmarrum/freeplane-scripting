8c87aefcd2a826ee6e6b9ac968e7a4791558a632
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-20 17:38:14 +0200

    new - FP#1161

---
+++ src/scripts/filterShowNextAncestorForSelected.groovy
@@ -0,0 +1,20 @@
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac2"})
+// https://github.com/freeplane/freeplane/issues/1161
+
+
+import org.freeplane.api.Node
+import org.freeplane.features.filter.FilterController
+import org.freeplane.plugin.script.proxy.ScriptUtils
+
+def filterController = FilterController.currentFilterController
+def showAncestors = filterController.showAncestors.selected
+if (!showAncestors) {
+    def c = ScriptUtils.c()
+    def node = ScriptUtils.node()
+    def showDescendants = filterController.showDescendants.selected
+    // for each selected node, find its next invisible ancestor
+    def expandedFilterHash = c.selecteds.collect().collect(new HashSet<Node>()) { it.pathToRoot.reverse().find { !it.visible } }
+    // add all nodes that are already visible
+    expandedFilterHash.addAll(node.mindMap.root.find { it.visible })
+    node.mindMap.filter(showAncestors, showDescendants, { it in expandedFilterHash })
+}
