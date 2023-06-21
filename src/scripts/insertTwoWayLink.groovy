3260a81683f298a8ee8ad2fa2be947d7284ed5ae
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-21 23:18:23 +0200

    new - FP#1255

---
+++ src/scripts/insertTwoWayLink.groovy
@@ -0,0 +1,10 @@
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac2/Insert"})
+// https://github.com/freeplane/freeplane/discussions/1255
+
+def nodes = c.selecteds.collect()
+if (nodes.size() != 2)
+    c.statusInfo = "got ${nodes.size()} nodes - expected 2"
+else {
+    nodes[0].link.node = nodes[1]
+    nodes[1].link.node = nodes[0]
+}
