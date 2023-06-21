c99791d77cd8ce5708c70b5659dc273bbee8dd88
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-21 23:27:04 +0200

    refactor

--- src/scripts/insertTwoWayLink.groovy
+++ src/scripts/insertTwoWayLink.groovy
@@ -2,9 +2,9 @@
 // https://github.com/freeplane/freeplane/discussions/1255
 
 def nodes = c.selecteds.collect()
-if (nodes.size() != 2)
-    c.statusInfo = "got ${nodes.size()} nodes - expected 2"
-else {
+if (nodes.size() == 2) {
     nodes[0].link.node = nodes[1]
     nodes[1].link.node = nodes[0]
+} else {
+    c.statusInfo = "got ${nodes.size()} nodes - expected 2"
 }
