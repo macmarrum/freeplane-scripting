a230b56bfd3da7de05670f8fb1b8319e4921734e
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-30 19:00:17 +0200

    fix dateTimeFormat
    use TextUtils to get DatePatter also when in Preferences it's specified descriptively (SHORT,SHORT)
    remove and add new attribute to always use the latest dateTimeFormat, otherwise the old format stays with the attribute

--- src/lib/MacmarrumNodeChangeListenerForModified.groovy
+++ src/lib/MacmarrumNodeChangeListenerForModified.groovy
@@ -1,20 +1,21 @@
 // https://github.com/freeplane/freeplane/issues/727
+
+
 import org.freeplane.api.Node
 import org.freeplane.api.NodeChangeListener
 import org.freeplane.api.NodeChanged
-import org.freeplane.plugin.script.FreeplaneScriptBaseClass.ConfigProperties
-
-import java.time.LocalDateTime
+import org.freeplane.core.util.TextUtils
 
 import static org.freeplane.api.NodeChanged.ChangedElement
 
 class MacmarrumNodeChangeListenerForModified implements NodeChangeListener {
     public static canReact = true
-    private static dateTimeFormat = new ConfigProperties().getProperty('datetime_format')
+    private static dateTimeFormat = new TextUtils().defaultDateTimeFormat
     private static ATTR_NAME = 'Modified'
 
     static void updateAttrModified(Node n) {
-        n[ATTR_NAME] = LocalDateTime.now().format(dateTimeFormat)
+        n[ATTR_NAME] = null
+        n[ATTR_NAME] = dateTimeFormat.format(new Date())
     }
 
     void nodeChanged(NodeChanged event) {
