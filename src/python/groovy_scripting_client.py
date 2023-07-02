2e1136a9014007029dae56acc96d084e65b7c35e
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-07-02 10:50:17 +0200

    add groovy_scripting_client.py
    that works with groovyScriptingServer.groovy

---
+++ src/python/groovy_scripting_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+import socket
+
+port = 48112
+encoding = 'UTF-8'
+
+
+def transfer(text: str) -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', port))
+    s.sendall(text.encode(encoding))
+    s.shutdown(socket.SHUT_WR)
+    response = []
+    response_chunk = b'x'
+    while response_chunk:
+        response_chunk = s.recv(1024)
+        response.append(response_chunk.decode(encoding))
+    s.close()
+    return ''.join(response)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        data = sys.argv[1]
+    else:
+        data = sys.stdin.read()
+    print(transfer(data))
