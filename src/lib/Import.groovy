99cce7a73b43cf46ca08b8d390ba664ed80a884d
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-07-02 14:08:32 +0200

    expand, add shouldFold
    fold to speed up and minimize the risk of view interactions during execution

--- src/lib/Import.groovy
+++ src/lib/Import.groovy
@@ -2,43 +2,85 @@ package io.github.macmarrum.freeplane
 
 import groovy.json.JsonParserType
 import groovy.json.JsonSlurper
-import org.freeplane.api.Node as FN
+import org.freeplane.api.Node
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
 class Import {
+    private static String DETAILS = '@details'
+    private static String ATTRIBUTES = '@attributes'
+    private static String NOTE = '@note'
 
     static String decodeBase64(String base64) {
         return new String(base64.decodeBase64())
     }
 
-    static void importJsonBase64(String base64, FN parent = null) {
-        importJson(decodeBase64(base64), parent)
+    static Node importJsonBase64(String base64, Node parent = null, boolean shouldFold = true) {
+        return importJson(decodeBase64(base64), parent, shouldFold)
     }
 
-    static void importJson(String content, FN parent = null) {
+    static Node importJson(String content, Node parent = null, boolean shouldFold = true) {
         if (!parent)
             parent = ScriptUtils.node().mindMap.root.createChild('JSON')
-        def jMap = new JsonSlurper(type: JsonParserType.INDEX_OVERLAY).parseText(content) as Map
-        importMapRecursively(jMap, parent)
+        fold(shouldFold, parent)
+        def jObject = new JsonSlurper(type: JsonParserType.INDEX_OVERLAY).parseText(content)
+        if (jObject instanceof Map)
+            importMapRecursively(jObject, parent)
+        else if (jObject instanceof List)
+            importList(jObject, parent)
+        unfold(shouldFold, parent)
+        return parent
     }
 
-    static void importMapRecursively(Map<String, Object> jMap, FN node) {
+    private static void fold(boolean shouldFold, Node parent) {
+        if (shouldFold && !parent.root) {
+            def child = parent.createChild('delete me')
+            parent.folded = true
+            child.delete()
+        }
+    }
+
+    private static void unfold(boolean shouldFold, Node parent) {
+        if (shouldFold && !parent.root) {
+            parent.folded = false
+        }
+    }
+
+    static void importMapRecursively(Map<String, Object> jMap, Node node) {
         jMap.each { key, value ->
-            if (value instanceof Map) {
+            if (key == DETAILS && value !instanceof Map && value !instanceof List) {
+                node.details = value
+            } else if (key == ATTRIBUTES && value instanceof Map) {
+                value.each { attrName, attrValue -> node[attrName as String] = attrValue }
+            } else if (key == NOTE && value !instanceof Map && value !instanceof List) {
+                node.note = value
+            }else if (value instanceof Map) {
                 def n = node.createChild(key)
                 importMapRecursively(value as Map, n)
             } else if (value instanceof List) {
                 def n = node.createChild(key)
-                value.eachWithIndex { elem, i ->
-                    if (elem instanceof Map) {
-                        importMapRecursively(elem as Map, n)
-                    } else {
-                        n.createChild(elem)
-                    }
-                }
+                importList(value, n)
             } else {
-                node[key] = value
+                def n = node.createChild(key)
+                if (value instanceof Map) {
+                    importMapRecursively(value as Map, n)
+                } else if (value instanceof List) {
+                    importList(value, n)
+                } else if (value !== null) {
+                    def child = n.createChild()
+                    child.text = value
+                }
             }
         }
+    }
+
+    static void importList(List list, Node node) {
+            list.each {
+                if (it instanceof Map) {
+                    importMapRecursively(it, node)
+                } else {
+                    def child = node.createChild()
+                    child.text = it
+                }
+            }
     }
 }
