d2032dc62928586f53fcd0cc835f3b7d772d9d4c
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-07-02 23:13:42 +0200

    use reminder for script execution; refactor

--- src/init/groovyScriptingServer.groovy
+++ src/init/groovyScriptingServer.groovy
@@ -1,19 +1,34 @@
 // https://github.com/freeplane/freeplane/discussions/792
+
 import org.freeplane.core.util.LogUtils
-import org.freeplane.plugin.script.ExecuteScriptException
-import org.freeplane.plugin.script.ScriptingEngine
-
-def port = 48112
-def shouldPrintScript = false
-
-def scriptHeader = '''\
 import org.freeplane.plugin.script.proxy.ScriptUtils
-def node = ScriptUtils.node()
+
+/* Set PORT to 0 for a random one
+ * Set HOSTNAME to '0.0.0.0' to enable access from outside
+ * Set SHOULD_PRINT_SCRIPT to see in Freeplane console the script being executed;
+ *     keep in mind to start Freeplane with a visible console, e.g. with freeplaneConsole.exe
+ */
+final PORT = System.getenv('FREEPLANE_GROOVY_SCRIPTING_SERVER_PORT') as Integer ?: 48112
+final ADDRESS = System.getenv('FREEPLANE_GROOVY_SCRIPTING_SERVER_ADDRESS') ?: '127.0.0.1'
+final SHOULD_PRINT_SCRIPT = System.getenv('FREEPLANE_GROOVY_SCRIPTING_SERVER_PRINT_SCRIPT') == '1'
+
+final SCRIPT_HEADER = '''\
+def __groovyScriptingServerReminderNode = node
 '''
+final SCRIPT_FOOTER = '''\
+if (__groovyScriptingServerReminderNode)
+    __groovyScriptingServerReminderNode.reminder.remove()
+'''
+final REMINDER_AFTER_MILLIS = 100
+final REMINDER_PERIOD_UNIT = 'YEAR'
+final REMINDER_PERIOD = 999
+
+final SCHEDULED = 'SCHEDULED'
+final ERROR = 'ERROR'
 
 new Thread(() -> {
     def server = new ServerSocket()
-    server.bind(new InetSocketAddress('127.0.0.1', port), 1)
+    server.bind(new InetSocketAddress(ADDRESS, PORT), 1)
     LogUtils.info("Groovy scripting server sterted on port ${server.localPort}")
 
     while (true) {
@@ -24,19 +39,30 @@ new Thread(() -> {
                 // and, apparently because of that,
                 // output cannot be written back to socket
                 def br = new BufferedReader(new InputStreamReader(input))
-                def sb = new StringBuilder(scriptHeader)
+                def sb = new StringBuilder(SCRIPT_HEADER)
                 String line
                 while ((line = br.readLine()) !== null) {
                     sb << line << '\n'
                 }
+                sb << SCRIPT_FOOTER
                 def script = sb.toString()
-                if (shouldPrintScript)
+                if (SHOULD_PRINT_SCRIPT)
                     print(script)
                 try {
-                    ScriptingEngine.executeScript(null, script)
-                    output.withWriter { it << 'OK' }
-                } catch (ExecuteScriptException e) {
-                    output.withPrintWriter { e.printStackTrace(it) }
+                    def reminder = ScriptUtils.node().reminder
+                    reminder.remove()
+                    def remindAt = new Date(Calendar.instance.timeInMillis + REMINDER_AFTER_MILLIS)
+                    reminder.createOrReplace(remindAt, REMINDER_PERIOD_UNIT, REMINDER_PERIOD)
+                    reminder.script = script
+                    output.withWriter {
+                        it << SCHEDULED
+                    }
+                } catch (Exception e) {
+                    output.withPrintWriter {
+                        it.println(ERROR)
+                        e.printStackTrace(it)
+                    }
+                    LogUtils.warn(e)
                 }
             }
         }
