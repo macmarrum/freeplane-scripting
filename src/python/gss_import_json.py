4c16c12ee58ac6fadcd059f95b6bd33f1002a6ed
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-07-02 14:16:20 +0200

    add date-time as parent, select parent

--- src/python/gss_import_json.py
+++ src/python/gss_import_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import base64
+from datetime import datetime
 from pathlib import Path
 
 from groovy_scripting_client import transfer
@@ -14,7 +15,10 @@ json_data = json_path.read_text(encoding=encoding)
 json_base64 = base64.b64encode(json_data.encode(encoding)).decode('L1')
 groovy_script = f"""
 import io.github.macmarrum.freeplane.Import
-Import.importJsonBase64('{json_base64}', node.mindMap.root)
+def parent = node.mindMap.root.createChild()
+parent.text = '{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+def n = Import.importJsonBase64('{json_base64}', parent)
+c.select(n)
 """
 response = transfer(groovy_script)
 print(response)
