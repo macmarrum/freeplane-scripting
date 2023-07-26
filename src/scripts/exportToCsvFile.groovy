dc17c7fd2fe86a5a423ccfb84f9b7b4c86346e31
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-07-26 22:42:32 +0200

    new

---
+++ src/scripts/exportToCsvFile.groovy
@@ -0,0 +1,12 @@
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Export"})
+
+
+import io.github.macmarrum.freeplane.Export
+import org.freeplane.plugin.script.proxy.ScriptUtils
+
+def node = ScriptUtils.node()
+
+def suggestedFile = new File(node.mindMap.file.path.replaceAll(/\.mm$/, '.csv'))
+def file = Export.askForFile(suggestedFile)
+println(":: ${new Date().format('yyyy-MM-dd HH:mm:ss')} exportToCsvFile `${file.path}'")
+Export.toCsvFile(file, node)
