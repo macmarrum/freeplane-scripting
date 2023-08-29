bcf0df853169b6341c5f44348d27b67fbe566199
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-08-30 00:40:55 +0200

    refactor

--- src/scripts/compareMindmap.groovy
+++ src/scripts/compareMindmap.groovy
@@ -43,21 +43,24 @@ static File askForFile() {
  */
 static void compare(MindMap newMindMap, MindMap oldMindMap) {
     println(":: compare(${newMindMap.name}, ${oldMindMap.name})")
-    assert newMindMap.root.id == oldMindMap.root.id
-    compareNodeRecursively(newMindMap.root, oldMindMap)
-    def c = ScriptUtils.c()
-    oldMindMap.root.findAll().each { Node it ->
-        if (!newMindMap.node(it.id)) {
-            def parent = newMindMap.node(it.parent.id)
-            def n = parent.appendChild(it)
-            n.left = it.left
-            n.moveTo(parent, it.parent.getChildPosition(it))
-            // only add style if parent wasn't deleted too
-            def pcs = parent.conditionalStyles.collect()
-            if (!pcs || !pcs.find { it.always && it.styleName == 'compare-mindmap:deleted' && it.active && !it.last }) {
-                n.conditionalStyles.insert(0, true, null, 'compare-mindmap:deleted', false)
-                c.select(n)
-                MenuUtils.executeMenuItems(['AddConnectorAction'])
+    if (newMindMap.root.id != oldMindMap.root.id) {
+        UITools.showMessage('Mind maps have different root nodes', JOptionPane.ERROR_MESSAGE)
+    } else {
+        compareNodeRecursively(newMindMap.root, oldMindMap)
+        def c = ScriptUtils.c()
+        oldMindMap.root.findAll().each { Node it ->
+            if (!newMindMap.node(it.id)) {
+                def parent = newMindMap.node(it.parent.id)
+                def n = parent.appendChild(it)
+                n.left = it.left
+                n.moveTo(parent, it.parent.getChildPosition(it))
+                // only add style if parent wasn't deleted too
+                def pcs = parent.conditionalStyles.collect()
+                if (!pcs || !pcs.find { it.always && it.styleName == 'compare-mindmap:deleted' && it.active && !it.last }) {
+                    n.conditionalStyles.insert(0, true, null, 'compare-mindmap:deleted', false)
+                    c.select(n)
+                    MenuUtils.executeMenuItems(['AddConnectorAction'])
+                }
             }
         }
     }
@@ -85,24 +88,18 @@ static void compareNodeRecursively(Node node, MindMap oldMindMap) {
             node.conditionalStyles.insert(0, true, null, 'compare-mindmap:changed-note', false)
             MenuUtils.executeMenuItems(['AddConnectorAction'])
         }
-        def oChildIdToPosition = new HashMap<String, Integer>()
-        oldNode.children.eachWithIndex { Node oChild, int oPos -> oChildIdToPosition.put(oChild.id, oPos) }
-        node.children.eachWithIndex { Node nChild, int nPos ->
-            if (!oChildIdToPosition.containsKey(nChild.id)) {
-                if (oldMindMap.node(nChild.id)) {
-                    // parent was different
-                    c.select(nChild)
-                    nChild.conditionalStyles.insert(0, true, null, 'compare-mindmap:moved-parent', false)
-                    MenuUtils.executeMenuItems(['AddConnectorAction'])
-                }
-            } else {
-                if (nPos != oChildIdToPosition[nChild.id]) {
-                    // node position was different
-                    c.select(nChild)
-                    nChild.conditionalStyles.insert(0, true, null, 'compare-mindmap:moved-position', false)
-                    MenuUtils.executeMenuItems(['AddConnectorAction'])
-                }
+        if (!node.root) {
+            if (node.parent.id != oldNode.parent.id) {
+                // parent changed
+                node.conditionalStyles.insert(0, true, null, 'compare-mindmap:moved-parent', false)
+                MenuUtils.executeMenuItems(['AddConnectorAction'])
+            } else if (node.parent.getChildPosition(node) != oldNode.parent.getChildPosition(oldNode)) {
+                // position changed
+                node.conditionalStyles.insert(0, true, null, 'compare-mindmap:moved-position', false)
+                MenuUtils.executeMenuItems(['AddConnectorAction'])
             }
+        }
+        node.children.each { Node nChild ->
             compareNodeRecursively(nChild, oldMindMap)
         }
     }
@@ -113,11 +110,9 @@ def oldFile = askForFile()
 if (oldFile) {
     def file = node.mindMap.file
     def diffFile = new File(file.parent, file.name.replaceAll(/\.mm$/, '~diff.mm'))
+    c.openMindMaps.find { MindMap it -> it.file == diffFile}.each { MindMap it -> it.close(true, false) /* force, dis-allowInteraction */ }
     Files.copy(node.mindMap.file.toPath(), diffFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
-    def diffLoader = c.mapLoader(diffFile).withView()
-    def diffMindMap = diffLoader.mindMap
+    def diffMindMap = c.mapLoader(diffFile).withView().mindMap
     def oldMindMap = c.mapLoader(oldFile).mindMap
     compare(diffMindMap, oldMindMap)
-//    def allowInteraction = true
-//    diffMindMap.save(allowInteraction)
 }
