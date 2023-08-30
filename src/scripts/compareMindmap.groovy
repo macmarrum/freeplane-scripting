1e2536b62a27c985b0b68cdbd9ee486f090412ea
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-08-30 19:06:22 +0200

    move to class; only add styles if missing

--- src/scripts/compareMindmap.groovy
+++ src/scripts/compareMindmap.groovy
@@ -18,144 +18,151 @@ import javax.swing.filechooser.FileNameExtensionFilter
 
 import static org.freeplane.features.styles.MapStyleModel.STYLES_USER_DEFINED
 
-def node = ScriptUtils.node()
-def c = ScriptUtils.c()
+class MindMapComparer {
+    public static String stylePrefix = 'diff:'
+    public static style = [
+            NEW        : "${stylePrefix}NEW",
+            DEL        : "${stylePrefix}DEL",
+            MV_parent  : "${stylePrefix}MV-parent",
+            MV_position: "${stylePrefix}MV-position",
+            CH_text    : "${stylePrefix}CH-text",
+            CH_details : "${stylePrefix}CH-details",
+            CH_note    : "${stylePrefix}CH-note",
+    ]
 
-static void createStyles(MapModel mapModel) {
-    createStyleWithConnector(mapModel, 'diff:NEW', 'NEW', '#52D273', 'LINE')
-    createStyleWithConnector(mapModel, 'diff:DEL', 'DEL', '#f9556b', 'LINE')
-    createStyleWithConnector(mapModel, 'diff:MV-parent', 'MV-parent', '#e57255ff', 'LINE')
-    createStyleWithConnector(mapModel, 'diff:MV-position', 'MV-position', '#e57255ff', 'LINE')
-    createStyleWithConnector(mapModel, 'diff:CH-text', 'CH-text', '#e5c453', 'CUBIC_CURVE')
-    createStyleWithConnector(mapModel, 'diff:CH-details', 'CH-details', '#d349a4', 'LINEAR_PATH')
-    createStyleWithConnector(mapModel, 'diff:CH-note', 'CH-note', '#46bddf', 'LINEAR_PATH')
-}
-
-static NodeModel getOrCreateUserDefStyle(MapModel mapModel, IStyle iStyle) {
-    def mapStyleModel = MapStyleModel.getExtension(mapModel)
-    def styleNode = mapStyleModel.getStyleNode(iStyle)
-    if (!styleNode) {
-        styleNode = new NodeModel(mapModel)
-        styleNode.setUserObject(iStyle)
-        def userStyleParentNode = mapStyleModel.getStyleNodeGroup(mapStyleModel.styleMap, STYLES_USER_DEFINED)
-//        def userStyleParentNode = mapStyleModel.styleMap.root.children.find { (it.userObject as StyleTranslatedObject).object == STYLES_USER_DEFINED }
-//        (Controller.currentModeController.mapController as MMapController).insertNode(styleNode, userStyleParentNode) // event triggered
-        userStyleParentNode.insert(styleNode, userStyleParentNode.childCount) // no event triggered
-        mapStyleModel.addStyleNode(styleNode)
-    }
-    return styleNode
-}
-
-static void createStyleWithConnector(MapModel mapModel, String styleName, String middleLabel, String colorCode, String shape) {
-    def nodeModel = getOrCreateUserDefStyle(mapModel, StyleFactory.create(styleName))
-    def n = new NodeProxy(nodeModel, null)
-    if (!n.connectorsIn) {
-        def conn = n.addConnectorTo(n)
-        conn.middleLabel = middleLabel
-        conn.colorCode = colorCode
-        conn.shape = shape
-        conn.startArrow = true
-        conn.endArrow = false
-    }
-}
-
-static File askForFile() {
-    final fileChooser = new JFileChooser()
-    fileChooser.dialogTitle = 'Select old mind map to compare'
-    fileChooser.multiSelectionEnabled = false
-    fileChooser.fileFilter = new FileNameExtensionFilter('Mind map', 'mm')
-    fileChooser.currentDirectory = ScriptUtils.node().mindMap.file.parentFile
-    final returnVal = fileChooser.showOpenDialog(UITools.currentRootComponent)
-    if (returnVal != JFileChooser.APPROVE_OPTION) {
-        return
-    }
-    return fileChooser.getSelectedFile()
-}
-
-/**
- * Compare two mind maps. Mark differences in newMindMap:
- * new
- * deleted
- * moved
- * changed core
- * changed details
- * changed note
- * 
- * Not implemented:
- * changed attributes
- * changed icons
- * changed style
- */
-static void compare(MindMap newMindMap, MindMap oldMindMap) {
-    if (newMindMap.root.id != oldMindMap.root.id) {
-        UITools.showMessage('Mind maps have different root nodes', JOptionPane.ERROR_MESSAGE)
-    } else {
-        compareNodeRecursively(newMindMap.root, oldMindMap)
-        def c = ScriptUtils.c()
-        oldMindMap.root.findAll().each { Node it ->
-            if (!newMindMap.node(it.id)) {
-                def parent = newMindMap.node(it.parent.id)
-                def n = parent.appendChild(it)
-                n.left = it.left
-                n.moveTo(parent, it.parent.getChildPosition(it))
-                // only add style if parent wasn't deleted too
-                def pcs = parent.conditionalStyles.collect()
-                if (!pcs || !pcs.find { it.always && it.styleName == 'diff:DEL' && it.active && !it.last }) {
-                    n.conditionalStyles.insert(0, true, null, 'diff:DEL', false)
-                    c.select(n)
-                    MenuUtils.executeMenuItems(['AddConnectorAction'])
+    /**
+     * Compare two mind maps. Mark differences in newMindMap:
+     * new node
+     * deleted node
+     * moved node
+     * changed core
+     * changed details
+     * changed note
+     */
+    static void compare(MindMap newMindMap, MindMap oldMindMap) {
+        createStylesIfMissing(newMindMap.delegate)
+        if (newMindMap.root.id != oldMindMap.root.id) {
+            UITools.showMessage('Mind maps have different root nodes', JOptionPane.ERROR_MESSAGE)
+        } else {
+            compareNodeRecursively(newMindMap.root, oldMindMap)
+            def c = ScriptUtils.c()
+            oldMindMap.root.findAll().each { Node it ->
+                if (!newMindMap.node(it.id)) {
+                    def parent = newMindMap.node(it.parent.id)
+                    def n = parent.appendChild(it)
+                    n.left = it.left
+                    n.moveTo(parent, it.parent.getChildPosition(it))
+                    // only add style if parent wasn't deleted too
+                    def pcs = parent.conditionalStyles.collect()
+                    if (!pcs || !pcs.find { it.always && it.styleName == style.DEL && it.active && !it.last }) {
+                        n.conditionalStyles.insert(0, true, null, style.DEL, false)
+                        c.select(n)
+                        MenuUtils.executeMenuItems(['AddConnectorAction'])
+                    }
                 }
             }
         }
     }
-}
 
-static void compareNodeRecursively(Node node, MindMap oldMindMap) {
-    def c = ScriptUtils.c()
-    def oldNode = oldMindMap.node(node.id)
-    c.select(node)
-    if (oldNode == null) {
-        // no such node in oldMindMap
-        node.conditionalStyles.insert(0, true, null, 'diff:NEW', false)
-        MenuUtils.executeMenuItems(['AddConnectorAction'])
-    } else {
-        if (node.text != oldNode.text) {
-            node.conditionalStyles.insert(0, true, null, 'diff:CH-text', false)
+    static void compareNodeRecursively(Node node, MindMap oldMindMap) {
+        def c = ScriptUtils.c()
+        def oldNode = oldMindMap.node(node.id)
+        c.select(node)
+        if (oldNode == null) {
+            // no such node in oldMindMap
+            node.conditionalStyles.insert(0, true, null, style.NEW, false)
             MenuUtils.executeMenuItems(['AddConnectorAction'])
-        }
-        if (node.detailsText != oldNode.detailsText) {
-            node.conditionalStyles.insert(0, true, null, 'diff:CH-details', false)
-            MenuUtils.executeMenuItems(['AddConnectorAction'])
-        }
-        if (node.noteText != oldNode.noteText) {
-            node.conditionalStyles.insert(0, true, null, 'diff:CH-note', false)
-            MenuUtils.executeMenuItems(['AddConnectorAction'])
-        }
-        if (!node.root) {
-            if (node.parent.id != oldNode.parent.id) {
-                // parent changed
-                node.conditionalStyles.insert(0, true, null, 'diff:MV-parent', false)
-                MenuUtils.executeMenuItems(['AddConnectorAction'])
-            } else if (node.parent.getChildPosition(node) != oldNode.parent.getChildPosition(oldNode)) {
-                // position changed
-                node.conditionalStyles.insert(0, true, null, 'diff:MV-position', false)
+        } else {
+            if (node.text != oldNode.text) {
+                node.conditionalStyles.insert(0, true, null, style.CH_text, false)
                 MenuUtils.executeMenuItems(['AddConnectorAction'])
             }
-        }
-        node.children.each { Node nChild ->
-            compareNodeRecursively(nChild, oldMindMap)
+            if (node.detailsText != oldNode.detailsText) {
+                node.conditionalStyles.insert(0, true, null, style.CH_details, false)
+                MenuUtils.executeMenuItems(['AddConnectorAction'])
+            }
+            if (node.noteText != oldNode.noteText) {
+                node.conditionalStyles.insert(0, true, null, style.CH_note, false)
+                MenuUtils.executeMenuItems(['AddConnectorAction'])
+            }
+            if (!node.root) {
+                if (node.parent.id != oldNode.parent.id) {
+                    // parent changed
+                    node.conditionalStyles.insert(0, true, null, style.MV_parent, false)
+                    MenuUtils.executeMenuItems(['AddConnectorAction'])
+                } else if (node.parent.getChildPosition(node) != oldNode.parent.getChildPosition(oldNode)) {
+                    // position changed
+                    node.conditionalStyles.insert(0, true, null, style.MV_position, false)
+                    MenuUtils.executeMenuItems(['AddConnectorAction'])
+                }
+            }
+            node.children.each { Node nChild ->
+                compareNodeRecursively(nChild, oldMindMap)
+            }
         }
     }
+
+    static void createStylesIfMissing(MapModel mapModel) {
+        createDiffStyleIfMissing(mapModel, style.NEW, '#52D273', 'LINE')
+        createDiffStyleIfMissing(mapModel, style.DEL, '#f9556b', 'LINE')
+        createDiffStyleIfMissing(mapModel, style.MV_parent, '#e57255ff', 'LINE')
+        createDiffStyleIfMissing(mapModel, style.MV_position, '#e57255ff', 'LINE')
+        createDiffStyleIfMissing(mapModel, style.CH_text, '#e5c453', 'CUBIC_CURVE')
+        createDiffStyleIfMissing(mapModel, style.CH_details, '#d349a4', 'LINEAR_PATH')
+        createDiffStyleIfMissing(mapModel, style.CH_note, '#46bddf', 'LINEAR_PATH')
+    }
+
+    static void createDiffStyleIfMissing(MapModel mapModel, String styleName, String colorCode, String shape) {
+        def mapStyleModel = MapStyleModel.getExtension(mapModel)
+        def iStyle = StyleFactory.create(styleName)
+        def styleNode = mapStyleModel.getStyleNode(iStyle)
+        if (!styleNode) {
+            def nodeModel = createUserDefStyle(mapModel, mapStyleModel, iStyle)
+            def n = new NodeProxy(nodeModel, null)
+            def conn = n.addConnectorTo(n)
+            conn.middleLabel = styleName[stylePrefix.size()..-1]
+            conn.colorCode = colorCode
+            conn.shape = shape
+            conn.startArrow = true
+            conn.endArrow = false
+        }
+    }
+
+    static NodeModel createUserDefStyle(MapModel mapModel, MapStyleModel mapStyleModel, IStyle iStyle) {
+        NodeModel styleNode = new NodeModel(mapModel)
+        styleNode.setUserObject(iStyle)
+//        def userStyleParentNode = mapStyleModel.styleMap.root.children.find { (it.userObject as StyleTranslatedObject).object == STYLES_USER_DEFINED }
+        def userStyleParentNode = mapStyleModel.getStyleNodeGroup(mapStyleModel.styleMap, STYLES_USER_DEFINED)
+//        (Controller.currentModeController.mapController as MMapController).insertNode(styleNode, userStyleParentNode) // event triggered
+        userStyleParentNode.insert(styleNode, userStyleParentNode.childCount) // no event triggered
+        mapStyleModel.addStyleNode(styleNode)
+        return styleNode
+    }
+
+    static File askForFile() {
+        final fileChooser = new JFileChooser()
+        fileChooser.dialogTitle = 'Select old mind map to compare'
+        fileChooser.multiSelectionEnabled = false
+        fileChooser.fileFilter = new FileNameExtensionFilter('Mind map', 'mm')
+        fileChooser.currentDirectory = ScriptUtils.node().mindMap.file.parentFile
+        final returnVal = fileChooser.showOpenDialog(UITools.currentRootComponent)
+        if (returnVal != JFileChooser.APPROVE_OPTION) {
+            return
+        }
+        return fileChooser.getSelectedFile()
+    }
 }
+
 
 if (!config.getBooleanProperty('assignsNodeDependantStylesToNewConnectors'))
     UITools.showMessage('Enable "Preferences…->Defaults->Connectors->Assigns node dependant styles to new connectors" and try again', JOptionPane.WARNING_MESSAGE)
 else {
-    def oldFile = askForFile()
+    def node = ScriptUtils.node()
+    def c = ScriptUtils.c()
+    def oldFile = MindMapComparer.askForFile()
     if (oldFile) {
         def oldMindMap = c.mapLoader(oldFile).mindMap
         def mindMap = c.mapLoader(node.mindMap.file).unsetMapLocation().withView().mindMap
-        createStyles(mindMap.delegate)
-        compare(mindMap, oldMindMap)
+        MindMapComparer.compare(mindMap, oldMindMap)
     }
 }
