519ea6b8e413dbb9be0a3c77f4ac02e52ec635f9
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-01 18:43:01 +0200

    include transfer (from freeplane_remote.py)

--- src/python/freeplane-remote-import-json.py
+++ src/python/freeplane-remote-import-json.py
@@ -13,6 +13,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import os
+import socket
 import sys
 import base64
 from datetime import datetime
