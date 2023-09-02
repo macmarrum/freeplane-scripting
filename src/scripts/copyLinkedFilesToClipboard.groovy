a83628e2e1f490cd5f3772523da6b1526b2956da
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-02 23:47:23 +0200

    fix so that file paths no longer are prefixed with map directory when URI is without 'file:'
    by using a modified getFile(), which in Freeplane is buggy

--- src/scripts/copyLinkedFilesToClipboard.groovy
+++ src/scripts/copyLinkedFilesToClipboard.groovy
@@ -9,35 +9,62 @@ import java.util.List
 
 class FileTransferable implements Transferable {
     private static CRLF = '\r\n'
+    private static LF = '\n'
     private static uriListFlavor = new DataFlavor('text/uri-list;class=java.lang.String')
-    private List<File> listOfFiles
+    private List<File> files
 
-    FileTransferable(List listOfFiles) {
-        this.listOfFiles = listOfFiles
+    FileTransferable(List<File> files) {
+        this.files = files
     }
 
     @Override
     DataFlavor[] getTransferDataFlavors() {
-        return new DataFlavor[]{DataFlavor.javaFileListFlavor, uriListFlavor}
+        return new DataFlavor[]{DataFlavor.javaFileListFlavor, uriListFlavor, DataFlavor.stringFlavor}
     }
 
     @Override
     boolean isDataFlavorSupported(DataFlavor flavor) {
-        return DataFlavor.javaFileListFlavor == flavor || uriListFlavor == flavor
+        return DataFlavor.javaFileListFlavor == flavor || uriListFlavor == flavor || DataFlavor.stringFlavor == flavor
     }
 
     @Override
     Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
         return switch (flavor) {
-            case DataFlavor.javaFileListFlavor -> listOfFiles
-            case uriListFlavor -> listOfFiles.collect { it.toURI() }.join(CRLF)
+            case DataFlavor.javaFileListFlavor -> files
+            case uriListFlavor -> files.collect { it.toURI() }.join(CRLF) // rfc2483
+            case DataFlavor.stringFlavor -> files.collect { it.path }.join(LF)
             default -> throw new UnsupportedFlavorException(flavor)
         }
     }
 }
 
-def listOfFiles = ScriptUtils.c().selecteds.collect { it.link.file }.findAll()
-Toolkit.defaultToolkit.systemClipboard.setContents(new FileTransferable(listOfFiles), new ClipboardOwner() {
+/**
+ * A modified version of org.freeplane.plugin.script.proxy.LinkProxy#getFile()
+ * As of Freeplane v1.11.6, the original is buggy.
+ */
+static File getFile(URI uri, File mapFile) {
+    try {
+        if (uri == null)
+            return null
+        if (uri.scheme == null || uri.scheme == 'file') {
+            def file = new File(uri.path)
+            if (file.isAbsolute()) {
+                return file
+            } else {
+                return mapFile ? new File(mapFile.parent, uri.path) : null
+            }
+        } else {
+            return new File(uri)
+        }
+    }
+    catch (Exception e) {
+        return null
+    }
+}
+
+def mapFile = ScriptUtils.node().mindMap.file
+def files = ScriptUtils.c().selecteds.collect { getFile(it.link.uri, mapFile) }.findAll()
+Toolkit.defaultToolkit.systemClipboard.setContents(new FileTransferable(files), new ClipboardOwner() {
     @Override
     void lostOwnership(Clipboard clipboard, Transferable transferable) {}
 })
