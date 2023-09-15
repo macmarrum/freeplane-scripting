88212209ac1ef8ad059ffae9679e32ef6d676b13
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-15 23:06:37 +0200

    refresh view in ver prior to 11

--- src/scripts/jumpOutWithStyle.groovy
+++ src/scripts/jumpOutWithStyle.groovy
@@ -1,6 +1,8 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Jump"})
 
+
 import org.freeplane.api.ConditionalStyle
+import org.freeplane.core.util.FreeplaneVersion
 import org.freeplane.core.util.MenuUtils
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
@@ -13,3 +15,8 @@ ScriptUtils.c().viewRoot.conditionalStyles.findAll {
     it.remove()
 }
 MenuUtils.executeMenuItems(['JumpOutAction'])
+if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
+    // force view refresh
+    def node = ScriptUtils.node()
+    node.style.name = node.style.name
+}
