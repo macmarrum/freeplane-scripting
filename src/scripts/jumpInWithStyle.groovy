dfdd49608fa2a03037226b132bb802d3e9d051d4
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-15 23:06:22 +0200

    no-op for root; refresh view in ver prior to 11

--- src/scripts/jumpInWithStyle.groovy
+++ src/scripts/jumpInWithStyle.groovy
@@ -1,5 +1,7 @@
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Jump"})
 
+
+import org.freeplane.core.util.FreeplaneVersion
 import org.freeplane.core.util.MenuUtils
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
@@ -8,7 +10,13 @@ import org.freeplane.plugin.script.proxy.ScriptUtils
 def jumpInStyleName = 'JumpIn'
 
 def node = ScriptUtils.node()
-def styleAlreadyAdded = node.conditionalStyles.find { it.active && it.always && it.styleName == jumpInStyleName && !it.last }
-if (!styleAlreadyAdded)
-    node.conditionalStyles.insert(0, true, null, jumpInStyleName, false)
-MenuUtils.executeMenuItems(['JumpInAction'])
+if (!node.isRoot()) {
+    def styleAlreadyAdded = node.conditionalStyles.find { it.active && it.always && it.styleName == jumpInStyleName && !it.last }
+    if (!styleAlreadyAdded)
+        node.conditionalStyles.insert(0, true, null, jumpInStyleName, false)
+    MenuUtils.executeMenuItems(['JumpInAction'])
+    if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
+        // force view refresh
+        node.style.name = node.style.name
+    }
+}
