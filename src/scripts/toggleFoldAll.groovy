e40e789cc190b5627f2934a03b957bfa2783a31c
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-16 08:47:19 +0200

    FP#1405

---
+++ src/scripts/toggleFoldAll.groovy
@@ -0,0 +1,21 @@
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
+// https://github.com/freeplane/freeplane/issues/1405
+
+
+import org.freeplane.core.util.MenuUtils
+import org.freeplane.plugin.script.proxy.ScriptUtils
+
+def UNFOLD_ALL_ACTION = ['UnfoldAllAction']
+def FOLD_ALL_ACTION = ['FoldAllAction']
+def c = ScriptUtils.c()
+def selectedNodes = c.selecteds.collect()
+def topNodes = selectedNodes.findAll { it.parent !in selectedNodes }
+topNodes.each { node ->
+    c.select(node)
+    if (node.isFolded()) {
+        MenuUtils.executeMenuItems(UNFOLD_ALL_ACTION)
+    } else {
+        MenuUtils.executeMenuItems(FOLD_ALL_ACTION)
+    }
+}
+c.select(topNodes)
