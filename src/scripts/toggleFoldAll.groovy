4473bed9188e57f66b227a15a2f9db048c6c102d
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-17 13:47:23 +0200

    fix to also unfold root

--- src/scripts/toggleFoldAll.groovy
+++ src/scripts/toggleFoldAll.groovy
@@ -9,13 +9,23 @@ def UNFOLD_ALL_ACTION = ['UnfoldAllAction']
 def FOLD_ALL_ACTION = ['FoldAllAction']
 def c = ScriptUtils.c()
 def selectedNodes = c.selecteds.collect()
-def topNodes = selectedNodes.findAll { it.parent !in selectedNodes }
-topNodes.each { node ->
-    c.select(node)
-    if (node.isFolded()) {
+def root = selectedNodes[0].mindMap.root
+if (root in selectedNodes) {
+    c.select(root)
+    if (root.children.findAll { !it.leaf }.every { it.isFolded() }) {
         MenuUtils.executeMenuItems(UNFOLD_ALL_ACTION)
     } else {
         MenuUtils.executeMenuItems(FOLD_ALL_ACTION)
     }
+} else {
+    def topNodes = selectedNodes.findAll { it.parent !in selectedNodes }
+    topNodes.each { node ->
+        c.select(node)
+        if (node.isFolded()) {
+            MenuUtils.executeMenuItems(UNFOLD_ALL_ACTION)
+        } else {
+            MenuUtils.executeMenuItems(FOLD_ALL_ACTION)
+        }
+    }
+    c.select(topNodes)
 }
-c.select(topNodes)
