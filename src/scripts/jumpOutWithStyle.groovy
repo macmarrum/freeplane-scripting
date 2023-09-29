e8e9171949c8ab24835195278f9aea6059f9bc8b
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-29 23:01:02 +0200

    clearJumpInStyleAndBackgroundColor also after jump-out, if they exist

--- src/scripts/jumpOutWithStyle.groovy
+++ src/scripts/jumpOutWithStyle.groovy
@@ -9,21 +9,29 @@ import org.freeplane.plugin.script.proxy.ScriptUtils
 // put the name of your custom style - must be the same as in jumpInWithStyle.groovy
 def jumpInStyleName = 'JumpIn'
 
+static def clearJumpInStyleAndBackgroundColor(node, jumpInStyleName) {
+    def preJumpInBackgroundColorCode = node['preJumpInBackgroundColor']
+    if (preJumpInBackgroundColorCode) {
+        node.mindMap.backgroundColorCode = preJumpInBackgroundColorCode == 'null' ? null : preJumpInBackgroundColorCode
+        node['preJumpInBackgroundColor'] = null
+    }
+    node.conditionalStyles.findAll {
+        it.active && it.always && it.styleName == jumpInStyleName && !it.last
+    }.each { ConditionalStyle it ->
+        it.remove()
+    }
+}
+
 def node = ScriptUtils.node()
 def c = ScriptUtils.c()
-c.viewRoot.conditionalStyles.findAll {
-    it.active && it.always && it.styleName == jumpInStyleName && !it.last
-}.each { ConditionalStyle it ->
-    it.remove()
-}
 def viewRoot = c.viewRoot
-def jumpInBackgroundColorCode = viewRoot['jumpInBackgroundColor']
-if (jumpInBackgroundColorCode) {
-    node.mindMap.backgroundColorCode = jumpInBackgroundColorCode == 'null' ? null : jumpInBackgroundColorCode
-    viewRoot['jumpInBackgroundColor'] = null
-}
-MenuUtils.executeMenuItems(['JumpOutAction'])
-if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
-    // force view refresh
-    node.style.name = node.style.name
+if (viewRoot != node.mindMap.root) {
+    clearJumpInStyleAndBackgroundColor(viewRoot, jumpInStyleName)
+    MenuUtils.executeMenuItems(['JumpOutAction'])
+//    if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
+//        // force view refresh
+//        node.style.name = node.style.name
+//    }
+} else {
+    clearJumpInStyleAndBackgroundColor(node, jumpInStyleName)
 }
