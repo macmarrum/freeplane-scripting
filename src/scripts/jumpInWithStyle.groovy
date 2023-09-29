504cdf1ec32a3e7ed9b518be1e59d26448d759c0
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-09-29 23:00:27 +0200

    add shouldJumpInDeriveFromAutoLevelRoot; refactor

--- src/scripts/jumpInWithStyle.groovy
+++ src/scripts/jumpInWithStyle.groovy
@@ -7,17 +7,30 @@ import org.freeplane.features.icon.factory.IconStoreFactory
 import org.freeplane.features.map.NodeModel
 import org.freeplane.features.styles.MapStyleModel
 import org.freeplane.features.styles.StyleFactory
+import org.freeplane.features.styles.StyleTranslatedObject
+import org.freeplane.plugin.script.proxy.NodeProxy
 import org.freeplane.plugin.script.proxy.MapProxy
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
+import static org.freeplane.features.styles.MapStyleModel.STYLES_AUTOMATIC_LAYOUT
 import static org.freeplane.features.styles.MapStyleModel.STYLES_USER_DEFINED
+import static org.freeplane.features.styles.AutomaticLayoutController.AUTOMATIC_LAYOUT_LEVEL_ROOT
 
 // put the name of your custom style - it will be created if it doesn't exist
 def jumpInStyleName = 'JumpIn'
-// put the name of the icon or '' to skip icon
-def jumpInIconName = 'emoji-1F4A0'
+// set to true for JumpIn to take on Auto-level-Root style
+def shouldJumpInDeriveFromAutoLevelRoot = true
 // put the background color code or '' to disable
 def jumpInMapBackgroundColorCode = '#003333'
+
+String jumpInIconName
+if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 7)) {
+    // for version before 1.11.7, put the name of the icon or '' to skip icon
+    jumpInIconName = 'emoji-1F4CD' //'emoji-1F4A0'
+} else {
+    // since version 1.11.7, a pin icon is automatically shown after a jump-in
+    jumpInIconName = ''
+}
 
 def node = ScriptUtils.node()
 if (!node.isRoot()) {
@@ -26,16 +39,28 @@ if (!node.isRoot()) {
     def mapModel = (node.mindMap as MapProxy).delegate
     def mapStyleModel = MapStyleModel.getExtension(mapModel)
     def userStyleParentNode = mapStyleModel.getStyleNodeGroup(mapStyleModel.styleMap, STYLES_USER_DEFINED)
-    def jumpInStyleFound = userStyleParentNode.children.find { it.text == jumpInStyleName }
-    if (!jumpInStyleFound) {
-        def styleNodeModel = new NodeModel(mapModel)
+    def jumpInNodeModel = userStyleParentNode.children.find { it.text == jumpInStyleName }
+    if (!jumpInNodeModel) {
+        jumpInNodeModel = new NodeModel(mapModel)
         def iStyle = StyleFactory.create(jumpInStyleName)
-        styleNodeModel.setUserObject(iStyle)
-        userStyleParentNode.insert(styleNodeModel, userStyleParentNode.childCount) // no event triggered
-        mapStyleModel.addStyleNode(styleNodeModel)
+        jumpInNodeModel.setUserObject(iStyle)
+        userStyleParentNode.insert(jumpInNodeModel, userStyleParentNode.childCount) // no event triggered
+        mapStyleModel.addStyleNode(jumpInNodeModel)
         if (jumpInIconName) {
             def jumpInIcon = IconStoreFactory.ICON_STORE.getMindIcon(jumpInIconName)
-            styleNodeModel.addIcon(jumpInIcon)
+            jumpInNodeModel.addIcon(jumpInIcon)
+        }
+        if (shouldJumpInDeriveFromAutoLevelRoot) {
+            def automaticLayoutParentNode = mapStyleModel.getStyleNodeGroup(mapStyleModel.styleMap, STYLES_AUTOMATIC_LAYOUT)
+            if (automaticLayoutParentNode) {
+                def styleRoot = automaticLayoutParentNode.children.find {
+                    (it.userObject as StyleTranslatedObject).object.toString() == AUTOMATIC_LAYOUT_LEVEL_ROOT
+                }
+                if (styleRoot) {
+                    def styleNode = new NodeProxy(jumpInNodeModel, null)
+                    styleNode.conditionalStyles.add(true, null, styleRoot.text, false)
+                }
+            }
         }
     }
     def styleAlreadyAdded = node.conditionalStyles.find { it.active && it.always && it.styleName == jumpInStyleName && !it.last }
@@ -44,11 +69,11 @@ if (!node.isRoot()) {
     }
     if (jumpInMapBackgroundColorCode) {
         // save the original color to restore it on jump out
-        ScriptUtils.c().viewRoot['jumpInBackgroundColor'] = node.mindMap.backgroundColorCode ?: 'null'
+        ScriptUtils.c().viewRoot['preJumpInBackgroundColor'] = node.mindMap.backgroundColorCode ?: 'null'
         node.mindMap.backgroundColorCode = jumpInMapBackgroundColorCode
     }
-    if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
-        // force view refresh
-        node.style.name = node.style.name
-    }
+//    if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
+//        // force view refresh
+//        node.style.name = node.style.name
+//    }
 }
