101561efdf45489828c3be355107996ed3101b5f
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-02-06 12:55:30 +0100

    change settings.nl value
    not to replace NL with CR so that NL-containing values are auto-quoted and can be pasted into spreadsheet cells as multi-line values

--- src/scripts/exportToTsvClipboard.groovy
+++ src/scripts/exportToTsvClipboard.groovy
@@ -5,5 +5,9 @@ import io.github.macmarrum.freeplane.Export
 import org.freeplane.core.util.TextUtils
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
-def text = Export.toCsvString(ScriptUtils.node(), [sep: Export.TAB])
+def settings = [
+        sep: Export.TAB,
+        nl: null, // don't replace NL with CR so that NL-containing values are auto-quoted and can be pasted into spreadsheet cells as multi-line values
+]
+def text = Export.toCsvString(ScriptUtils.node(), settings)
 TextUtils.copyToClipboard(text)
