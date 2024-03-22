2465c9b3485e443736734837a700a33a9782fbc6
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-03-22 08:40:34 +0100

    fix and improve

--- src/scripts/selectLinkedNodeInAnotherView.groovy
+++ src/scripts/selectLinkedNodeInAnotherView.groovy
@@ -2,53 +2,69 @@
 // https://github.com/freeplane/freeplane/issues/316
 
 
+import org.freeplane.core.ui.components.UITools
 import org.freeplane.core.util.MenuUtils
+import org.freeplane.features.map.MapModel
+import org.freeplane.features.map.NodeModel
 import org.freeplane.features.mode.Controller
 import org.freeplane.features.url.NodeAndMapReference
 import org.freeplane.plugin.script.proxy.ScriptUtils
 import org.freeplane.view.swing.map.MapViewController
 
-def scriptName = 'selectLinkedNodeInAnotherView'
+import javax.swing.JOptionPane
 
 def c = ScriptUtils.c()
 def node = ScriptUtils.node()
-def targetNode = node.link.node
+def targetNode = node.link.node // resolves #ID and #at
 def linkText = node.link.text?.replaceAll($/^freeplane:/%20/$, '') // remove the prefix because it messes up nodeAndMapReference
-if (!targetNode && linkText) {
-    def nodeAndMapReference = new NodeAndMapReference(linkText)
-    if (nodeAndMapReference.hasFreeplaneFileExtension() && nodeAndMapReference.hasNodeReference()) {
-        def targetFile = new File(nodeAndMapReference.mapReference)
-        if (!targetFile.absolute)
-            targetFile = new File(node.mindMap.file.parent, nodeAndMapReference.mapReference)
-        if (node.mindMap.file.canonicalFile <=> targetFile.canonicalFile) {
-            // a different map - simply follow the link
-            c.statusInfo = "$scriptName:  $linkText "
-            MenuUtils.executeMenuItems(['FollowLinkAction'])
-            return
-        } else {
-            targetNode = node.mindMap.node(nodeAndMapReference.nodeReference)
+if (!targetNode) { // not the case of same-map target node or a full node URI is used as the link
+    // work out what the target node is
+    if (linkText && node.link.uri.scheme !in ['menuitem', 'file', 'https', 'http']) {
+        def nodeAndMapReference = new NodeAndMapReference(linkText)
+        if (nodeAndMapReference.hasFreeplaneFileExtension() && nodeAndMapReference.hasNodeReference()) {
+            def thisFile = node.mindMap.file
+            def targetFile = new File(nodeAndMapReference.mapReference)
+            if (!targetFile.absolute) {
+                if (!thisFile) {
+                    MenuUtils.executeMenuItems(['SaveAsAction'])
+                    thisFile = node.mindMap.file
+                    if (!thisFile) // Save-As action was aborted
+                        return
+                }
+                targetFile = new File(thisFile.parent, nodeAndMapReference.mapReference)
+            }
+            if (thisFile?.canonicalFile <=> targetFile.canonicalFile) {
+                // a different map - simply follow the link
+                c.statusInfo = "${this.class.simpleName}:  $linkText (following it) "
+                MenuUtils.executeMenuItems(['FollowLinkAction'])
+                return
+            } else {
+                targetNode = node.mindMap.node(nodeAndMapReference.nodeReference)
+            }
         }
+    } else { // synchronize views by switching to the same node in another view
+        targetNode = node // comment out this line to disable same-node view sync
+        linkText = "#${node.id} (the same node in another view) "
     }
-} else { // synchronize views by switching to the same node in another view
-    targetNode = node // comment out this line to disable view sync
-    linkText = "#${node.id} (in another view)"
 }
-if (targetNode) {
+if (targetNode) { // same-map target node
+    def targetNodeModel = targetNode.delegate as NodeModel
     // based on org.freeplane.plugin.script.proxy.ControllerProxy#getMapViewManager
     def mvc = Controller.currentController.mapViewManager as MapViewController
-    def thisMapModel = node.mindMap.delegate
+    def thisMapModel = node.mindMap.delegate as MapModel
     def thisMapView = mvc.mapView
     def otherMapView = mvc.mapViewVector.find { it !== thisMapView && it.model === thisMapModel }
-    if (otherMapView) {
-        otherMapView.select()
-        def nodeModel = targetNode.delegate
-        // based on org.freeplane.plugin.script.proxy.ControllerProxy#select(org.freeplane.api.Node)
-        Controller.getCurrentModeController().getMapController().displayNode(nodeModel)
-        Controller.getCurrentController().getSelection().selectAsTheOnlyOneSelected(nodeModel)
-        c.statusInfo = "$scriptName:  $linkText "
-    } else {
-        c.statusInfo = "$scriptName:  no other views found! "
+    if (!otherMapView) { // open another map view if not exists
+        Controller.getCurrentModeController().getMapController().createMapView(thisMapModel)
+        otherMapView = mvc.mapViewVector.find { it !== thisMapView && it.model === thisMapModel }
+        UITools.showMessage('A new map view has been created', JOptionPane.INFORMATION_MESSAGE)
     }
+    otherMapView.select()
+    // based on org.freeplane.plugin.script.proxy.ControllerProxy#select(org.freeplane.api.Node)
+    Controller.getCurrentModeController().getMapController().displayNode(targetNodeModel)
+    Controller.getCurrentController().getSelection().selectAsTheOnlyOneSelected(targetNodeModel)
+    c.statusInfo = "${this.class.simpleName}:  $linkText "
 } else {
-    c.statusInfo = "$scriptName:  no link found! "
+    c.statusInfo = "${this.class.simpleName}:  no link found! "
 }
+
