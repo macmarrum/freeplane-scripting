899328fdd301a5e2b115bb3cee9ce1148baeac92
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-04-06 15:01:44 +0200

    add licence; clean up & improve comments

--- src/scripts/jumpOutWithStyle.groovy
+++ src/scripts/jumpOutWithStyle.groovy
@@ -1,12 +1,27 @@
+/*
+ * Copyright (C) 2023, 2024  macmarrum
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Jump"})
 
 
 import org.freeplane.api.ConditionalStyle
-import org.freeplane.core.util.FreeplaneVersion
 import org.freeplane.core.util.MenuUtils
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
-// put the name of your custom style - must be the same as in jumpInWithStyle.groovy
+// specify the name of your custom style - must be the same as in jumpInWithStyle.groovy
 def jumpInStyleName = 'JumpIn'
 
 static def clearJumpInStyleAndBackgroundColor(node, jumpInStyleName) {
@@ -28,10 +43,6 @@ def viewRoot = c.viewRoot
 if (viewRoot != node.mindMap.root) {
     clearJumpInStyleAndBackgroundColor(viewRoot, jumpInStyleName)
     MenuUtils.executeMenuItems(['JumpOutAction'])
-//    if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
-//        // force view refresh
-//        node.style.name = node.style.name
-//    }
 } else {
     clearJumpInStyleAndBackgroundColor(node, jumpInStyleName)
 }
