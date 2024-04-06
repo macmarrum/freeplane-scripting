a4da0125ceab01bbd31784bdbc48ef7332344984
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-04-06 23:52:55 +0200

    add contact address; shorten connectors and lower their angle

--- src/lib/io/github/macmarrum/freeplane/MindMapComparator.groovy
+++ src/lib/io/github/macmarrum/freeplane/MindMapComparator.groovy
@@ -1,19 +1,19 @@
 /*
-Copyright (C) 2023  macmarrum
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2023, 2024  macmarrum (at) outlook (dot) ie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package io.github.macmarrum.freeplane
 
 
@@ -41,7 +41,7 @@ class MindMapComparator {
             CH_details : "${stylePrefix}CH-details",
             CH_note    : "${stylePrefix}CH-note",
     ]
-    public static inclinations = [[130, -60], [130, -20]]
+    public static inclinations = [[100, -40], [100, -10]]
     public static final ADD_CONNECTOR_ACTION_LIST = ['AddConnectorAction']
 
     /**
