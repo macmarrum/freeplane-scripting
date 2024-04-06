6505b77e249bdba0d6e9b83ae406fb6d132f5c07
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-04-06 15:01:23 +0200

    add licence; fix ALS root; clean up & improve comments

--- src/scripts/jumpInWithStyle.groovy
+++ src/scripts/jumpInWithStyle.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023, 2024  macmarrum
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Jump"})
 
 
@@ -8,24 +24,28 @@ import org.freeplane.features.map.NodeModel
 import org.freeplane.features.styles.MapStyleModel
 import org.freeplane.features.styles.StyleFactory
 import org.freeplane.features.styles.StyleTranslatedObject
-import org.freeplane.plugin.script.proxy.NodeProxy
 import org.freeplane.plugin.script.proxy.MapProxy
+import org.freeplane.plugin.script.proxy.NodeProxy
 import org.freeplane.plugin.script.proxy.ScriptUtils
 
 import static org.freeplane.features.styles.MapStyleModel.STYLES_AUTOMATIC_LAYOUT
 import static org.freeplane.features.styles.MapStyleModel.STYLES_USER_DEFINED
-import static org.freeplane.features.styles.AutomaticLayoutController.AUTOMATIC_LAYOUT_LEVEL_ROOT
 
-// put the name of your custom style - it will be created if it doesn't exist
+//import static org.freeplane.features.styles.AutomaticLayoutController.AUTOMATIC_LAYOUT_LEVEL_ROOT
+// AutomaticLayoutController.AUTOMATIC_LAYOUT_LEVEL_ROOT is private
+final AUTOMATIC_LAYOUT_LEVEL_ROOT = 'AutomaticLayout.level.root'
+
+// specify the name of your custom style - it will be created if it doesn't exist
 def jumpInStyleName = 'JumpIn'
 // set to true for JumpIn to take on Auto-level-Root style
 def shouldJumpInDeriveFromAutoLevelRoot = true
-// put the background color code or '' to disable
+// specify the background color code or '' to disable
 def jumpInMapBackgroundColorCode = '#003333'
 
 String jumpInIconName
 if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 7)) {
-    // for version before 1.11.7, put the name of the icon or '' to skip icon
+    // for version before 1.11.7, which doesn't yet display a pin for jump-in
+    // specify the name of the icon or '' to skip icon
     jumpInIconName = 'emoji-1F4CD' //'emoji-1F4A0'
 } else {
     // since version 1.11.7, a pin icon is automatically shown after a jump-in
@@ -72,8 +92,4 @@ if (!node.isRoot()) {
         ScriptUtils.c().viewRoot['preJumpInBackgroundColor'] = node.mindMap.backgroundColorCode ?: 'null'
         node.mindMap.backgroundColorCode = jumpInMapBackgroundColorCode
     }
-//    if (FreeplaneVersion.version < new FreeplaneVersion(1, 11, 1)) {
-//        // force view refresh
-//        node.style.name = node.style.name
-//    }
 }
