f42a192c3b460b6b339d5aa2276ff7bd18523e28
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-04-09 10:17:20 +0200

    add copyright and license notice to ALS Styles

--- src/templates/macmarrum-dark.mm
+++ src/templates/macmarrum-dark.mm
@@ -846,7 +846,23 @@ ol {
     
   </head>
   <body>
-    <pre>import groovy.json.JsonSlurper
+    <pre>/*
+ * Copyright (C) 2024  macmarrum (at) outlook (dot) ie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ */
+import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 import org.freeplane.api.Node
 import org.freeplane.core.util.MenuUtils
@@ -879,7 +895,8 @@ class Styles {
     }
 }</pre>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 <node TEXT="" ID="ID_433164527">
 <hook NAME="FirstGroupNode"/>
 </node>
