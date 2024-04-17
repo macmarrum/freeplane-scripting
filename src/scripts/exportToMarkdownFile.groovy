1579a9d0695e57a2b02ce6db81e51c323b7de560
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-04-18 00:14:10 +0200

    fix log message; add license notice

--- src/scripts/exportToMarkdownFile.groovy
+++ src/scripts/exportToMarkdownFile.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023, 2024  macmarrum (at) outlook (dot) ie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Export"})
 
 
@@ -8,5 +24,5 @@ def node = ScriptUtils.node()
 
 def suggestedFile = new File(node.mindMap.file.path.replaceAll(/\.mm$/, '.md'))
 def file = Export.askForFile(suggestedFile)
-println(":: ${new Date().format('yyyy-MM-dd HH:mm:ss')} exportToCsvFile `${file.path}'")
+println(":: ${new Date().format('yyyy-MM-dd HH:mm:ss')} exportToMarkdownFile `${file.path}'")
 Export.toMarkdownFile(file, node)
