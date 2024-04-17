88b9e3749f9228bc4de9134e1cc3c5bfde9a4c77
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-04-18 00:11:08 +0200

    rename: add Skip1; add license notice

---
+++ src/scripts/exportToMdTbodyClpbSkip1.groovy
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023, 2024  macmarrum (at) outlook (dot) ie
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Export"})
+
+
+import io.github.macmarrum.freeplane.Export
+import org.freeplane.core.util.TextUtils
+import org.freeplane.plugin.script.proxy.ScriptUtils
+
+def text = Export.toCsvString(ScriptUtils.node(), [sep: Export.PIPE, tail: true, skip: 1])
+TextUtils.copyToClipboard(text)
