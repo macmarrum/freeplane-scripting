bd6ff959c36450e5e3f9bbc491f2f17c50f67646
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-05-21 09:11:44 +0200

    ask for directory if not configured; refactor

--- src/scripts/cs_export_1_11_5_to_1_9_13.groovy
+++ src/scripts/cs_export_1_11_5_to_1_9_13.groovy
@@ -1,6 +1,9 @@
 // Copyright (C) 2024  macmarrum (at) outlook (dot) ie
 // SPDX-License-Identifier: GPL-3.0-or-later
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/CStorage"})
+
+
+import org.freeplane.api.Controller
 import org.freeplane.api.Node
 import org.freeplane.core.ui.components.UITools
 
@@ -9,13 +12,17 @@ import javax.xml.transform.TransformerFactory
 import javax.xml.transform.stream.StreamResult
 import javax.xml.transform.stream.StreamSource
 
-File mmFile = (node as Node).mindMap.file
-def outputDir = new File(config.getProperty('_confluence_storage_output_dir'))
+node = node as Node
+c = c as Controller
+
+def mmFile = node.mindMap.file
+String _confluence_storage_output_dir = config.getProperty('_confluence_storage_output_dir', 'ask')
+def outputDir = (_confluence_storage_output_dir == 'ask') ? askForDirectory(mmFile) : new File(_confluence_storage_output_dir)
 if (!outputDir.exists()) {
     UITools.showMessage("Output Directory \n${outputDir}\ndoesn't exist", JOptionPane.ERROR_MESSAGE)
     return
 }
-if (outputDir == mmFile.parent) {
+if (outputDir == mmFile.parentFile) {
     UITools.showMessage("Output Directory is the same as mind-map parent \n${outputDir}", JOptionPane.ERROR_MESSAGE)
     return
 }
@@ -37,4 +44,18 @@ outputFile.withOutputStream {
     TransformerFactory.newInstance()
             .newTransformer(new StreamSource(xsltFile))
             .transform(new StreamSource(mmFile), new StreamResult(it))
+}
+c.statusInfo = "exported to ${outputFile}"
+
+static File askForDirectory(File suggestedFile = null) {
+    final fileChooser = new JFileChooser()
+    fileChooser.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+    fileChooser.multiSelectionEnabled = false
+    if (suggestedFile)
+        fileChooser.selectedFile = suggestedFile
+    final returnVal = fileChooser.showOpenDialog(null)
+    if (returnVal != JFileChooser.APPROVE_OPTION) {
+        return
+    }
+    return fileChooser.getSelectedFile()
 }
