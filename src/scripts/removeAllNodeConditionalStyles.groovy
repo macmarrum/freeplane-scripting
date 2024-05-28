815fe625e1f15555a846c734f79c6ba9cf518f3d
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-05-28 20:27:02 +0200

    change to use Scripting API

--- src/scripts/removeAllNodeConditionalStyles.groovy
+++ src/scripts/removeAllNodeConditionalStyles.groovy
@@ -1,20 +1,14 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Remove"})
-//import org.freeplane.features.map.NodeModel
+/**
+ * Copyright (C) 2022, 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Style"})
 
+
+import org.freeplane.api.Controller
 import org.freeplane.api.Node
-import org.freeplane.features.styles.ConditionalStyleModel
-//import org.freeplane.features.styles.LogicalStyleController
 
-c.selecteds.each { Node node ->
-    String styleName = node.style.name
-    node.delegate.removeExtension(ConditionalStyleModel.class)
-    node.style.name = styleName
+def c = c as Controller
+c.selecteds.each { Node n ->
+    n.conditionalStyles.collect().each { it.remove() }
 }
-//NodeModel nodeModel = node.delegate
-//ConditionalStyleModel conditionalStyleModel = nodeModel.getExtension(ConditionalStyleModel.class)
-//int i = conditionalStyleModel.styles.size() - 1
-//while (i >= 0) {
-//    LogicalStyleController.controller.removeConditionalStyle(conditionalStyleModel, i)
-//    i--
-//}
-//node.style.name = node.style.name
