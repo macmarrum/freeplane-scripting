6364146bbe58b3e48e842ac231f049177096bb9a
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-06-18 13:46:17 +0200

    refactor

--- src/scripts/setSideDefault.groovy
+++ src/scripts/setSideDefault.groovy
@@ -1,2 +1,11 @@
+/*
+ * Copyright (C) 2022, 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac3"})
-c.selecteds.each { it.sideAtRoot = 'DEFAULT' }
+
+import org.freeplane.api.Controller
+import org.freeplane.api.Side
+
+def c = c as Controller
+c.selecteds.each { it.sideAtRoot = Side.DEFAULT }
