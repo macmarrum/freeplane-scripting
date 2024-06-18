bb46c4bd7533833963eb18ae8a1e3e4ca5ce5643
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-06-18 13:48:02 +0200

    fix to no longer crash in version >= 1.11.1

--- src/scripts/setSideLeft.groovy
+++ src/scripts/setSideLeft.groovy
@@ -1,2 +1,17 @@
+/*
+ * Copyright (C) 2022, 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac3"})
-c.selecteds.each { it.sideAtRoot = 'LEFT' }
+
+import org.freeplane.api.Controller
+import org.freeplane.api.Side
+
+def c = c as Controller
+Side left
+try {
+    left = Side.TOP_OR_LEFT
+} catch (MissingPropertyException ignored) {
+    left = Side.LEFT
+}
+c.selecteds.each { it.sideAtRoot = left }
--- src/scripts/setSideRight.groovy
+++ src/scripts/setSideRight.groovy
@@ -1,2 +1,17 @@
+/*
+ * Copyright (C) 2022, 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac3"})
-c.selecteds.each { it.sideAtRoot = 'RIGHT' }
+
+import org.freeplane.api.Controller
+import org.freeplane.api.Side
+
+def c = c as Controller
+Side right
+try {
+    right = Side.BOTTOM_OR_RIGHT
+} catch (MissingPropertyException ignored) {
+    right = Side.RIGHT
+}
+c.selecteds.each { it.sideAtRoot = right }
