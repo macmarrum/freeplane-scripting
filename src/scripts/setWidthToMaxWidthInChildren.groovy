aa9e88e974536e541c9dfbaa30c1c65b6e78dfde
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-06-27 23:40:43 +0200

    optimize for clones; add 10% buffer; add license notice; change menu location

--- src/scripts/setWidthToMaxWidthInChildren.groovy
+++ src/scripts/setWidthToMaxWidthInChildren.groovy
@@ -1,28 +1,54 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac3"})
-import org.freeplane.api.NodeRO as FPN
+/*
+ * Copyright (C) 2022, 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Width"})
 
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+import org.freeplane.features.map.NodeModel
 
-int getWidth(FPN node, float zoom) {
-    def v = node.delegate.viewers
+def c = c as Controller
+
+static findSinglesAndCloneLeaders(Iterable<Node> nodes) {
+    def singlesAndLeaders = new LinkedList<Node>()
+    def subtrees = new HashSet<Node>()
+    def clones = new HashSet<Node>()
+    nodes.each { Node n ->
+        if (n !in subtrees) { // not a tree clone or a clone leader
+            def nodesSharingContentAndSubtree = n.nodesSharingContentAndSubtree
+            subtrees.addAll(nodesSharingContentAndSubtree)
+            if (n !in clones) { // not a content clone or a clone leader
+                singlesAndLeaders << n
+                clones.addAll(n.nodesSharingContent - nodesSharingContentAndSubtree)
+            }
+        }
+    }
+    return singlesAndLeaders
+}
+
+static int getWidth(Node node, float zoom) {
+    def m = node.delegate as NodeModel
+    def v = m.viewers
     def viewCount = v.size()
     assert viewCount == 1, "Only one view is supported, got ${viewCount}"
-    double width = v[0].mainView.width / zoom
+    // add 1% as a buffer, because nodes and fonts scale differently, i.e.
+    // text in fixed-size nodes gets wrapped when monitor size changes (monitor_size_inches decrease)
+    double width = v[0].mainView.width / zoom * 1.01
     return Math.ceil(width)
 }
 
+def visibleSinglesAndCloneLeadersOfSelected = findSinglesAndCloneLeaders(c.selecteds*.children.flatten() as Iterable<Node>).findAll { it.isVisible() }
+
 float zoom = c.getZoom()
 int maxWidthInSelection = 0
-node.children.each { FPN it ->
-    if (it.visible) {
-        int w = getWidth(it, zoom)
-        if (w > maxWidthInSelection)
-            maxWidthInSelection = w
-    }
+visibleSinglesAndCloneLeadersOfSelected.each { Node n ->
+    int w = getWidth(n, zoom)
+    if (w > maxWidthInSelection)
+        maxWidthInSelection = w
 }
 
-node.children.each { FPN it ->
-    if (it.visible) {
-        it.style.maxNodeWidth = maxWidthInSelection
-        it.style.minNodeWidth = maxWidthInSelection
-    }
+visibleSinglesAndCloneLeadersOfSelected.each { Node n ->
+    n.style.maxNodeWidth = maxWidthInSelection
+    n.style.minNodeWidth = maxWidthInSelection
 }
--- src/scripts/setWidthToMaxWidthInSelection.groovy
+++ src/scripts/setWidthToMaxWidthInSelection.groovy
@@ -1,24 +1,54 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac3"})
-import org.freeplane.api.NodeRO as FPN
+/*
+ * Copyright (C) 2022, 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Width"})
 
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+import org.freeplane.features.map.NodeModel
 
-int getWidth(FPN node, float zoom) {
-    def v = node.delegate.viewers
+def c = c as Controller
+
+static findSinglesAndCloneLeaders(Iterable<Node> nodes) {
+    def singlesAndLeaders = new LinkedList<Node>()
+    def subtrees = new HashSet<Node>()
+    def clones = new HashSet<Node>()
+    nodes.each { Node n ->
+        if (n !in subtrees) { // not a tree clone or a clone leader
+            def nodesSharingContentAndSubtree = n.nodesSharingContentAndSubtree
+            subtrees.addAll(nodesSharingContentAndSubtree)
+            if (n !in clones) { // not a content clone or a clone leader
+                singlesAndLeaders << n
+                clones.addAll(n.nodesSharingContent - nodesSharingContentAndSubtree)
+            }
+        }
+    }
+    return singlesAndLeaders
+}
+
+static int getWidth(Node node, float zoom) {
+    def m = node.delegate as NodeModel
+    def v = m.viewers
     def viewCount = v.size()
     assert viewCount == 1, "Only one view is supported, got ${viewCount}"
-    double width = v[0].mainView.width / zoom
+    // add 1% as a buffer, because nodes and fonts scale differently, i.e.
+    // text in fixed-size nodes gets wrapped when monitor size changes (monitor_size_inches decrease)
+    double width = v[0].mainView.width / zoom * 1.01
     return Math.ceil(width)
 }
 
+def visibleSinglesAndCloneLeadersOfSelected = findSinglesAndCloneLeaders(c.selecteds)
+
 float zoom = c.getZoom()
 int maxWidthInSelection = 0
-c.selecteds.each { FPN it ->
+visibleSinglesAndCloneLeadersOfSelected.each { Node it ->
     int w = getWidth(it, zoom)
     if (w > maxWidthInSelection)
         maxWidthInSelection = w
 }
 
-c.selecteds.each { FPN it ->
+visibleSinglesAndCloneLeadersOfSelected.each { Node it ->
     it.style.maxNodeWidth = maxWidthInSelection
     it.style.minNodeWidth = maxWidthInSelection
 }
