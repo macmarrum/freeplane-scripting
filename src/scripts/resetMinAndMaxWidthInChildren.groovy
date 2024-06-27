70eadd85ae72d177ccddf11cdecd55b044bcf6d6
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-06-27 23:41:35 +0200

    optimize for clones; add license notice; change menu location

--- src/scripts/resetMinAndMaxWidthInChildren.groovy
+++ src/scripts/resetMinAndMaxWidthInChildren.groovy
@@ -1,9 +1,31 @@
-// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac3"})
-import org.freeplane.api.NodeRO as FPN
+/*
+ * Copyright (C) 2022, 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Width"})
 
-node.children.each { FPN it ->
-    if (it.visible) {
-        it.style.maxNodeWidth = -1
-        it.style.minNodeWidth = -1
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+
+def c = c as Controller
+
+static findSinglesAndCloneLeaders(Iterable<Node> nodes) {
+    def singlesAndLeaders = new LinkedList<Node>()
+    def subtrees = new HashSet<Node>()
+    def clones = new HashSet<Node>()
+    nodes.each { Node n ->
+        if (n !in subtrees) { // not a tree clone or a clone leader
+            def nodesSharingContentAndSubtree = n.nodesSharingContentAndSubtree
+            subtrees.addAll(nodesSharingContentAndSubtree)
+            if (n !in clones) { // not a content clone or a clone leader
+                singlesAndLeaders << n
+                clones.addAll(n.nodesSharingContent - nodesSharingContentAndSubtree)
+            }
+        }
     }
+    return singlesAndLeaders
+}
+findSinglesAndCloneLeaders(c.selecteds*.children.flatten() as Iterable<Node>).findAll { it.isVisible() }.each { Node n ->
+    n.style.maxNodeWidth = -1
+    n.style.minNodeWidth = -1
 }
