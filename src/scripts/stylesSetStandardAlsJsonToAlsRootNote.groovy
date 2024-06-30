5250530168e3898a22ec6acefa4893dfd2e1f226
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-07-01 00:11:16 +0200

    new

---
+++ src/scripts/stylesSetStandardAlsJsonToAlsRootNote.groovy
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2024  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Styles"})
+
+import io.github.macmarrum.freeplane.Styles
+
+Styles.setStandardAlsJsonToAlsRootNote()
