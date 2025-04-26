77526c254c0545408337fd58ca57e0e73b68fa09
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-04-26 12:53:31 +0200

    honor `Preferences…->Environment->Hyperlink types->Links` [relative|absolute] when pasting images

--- src/scripts/pasteImageSavingToCustomLocation.groovy
+++ src/scripts/pasteImageSavingToCustomLocation.groovy
@@ -4,6 +4,7 @@
  */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Paste"})
 
+
 import org.freeplane.api.Controller
 import org.freeplane.api.Node
 
@@ -14,18 +15,24 @@ import java.awt.image.BufferedImage
 
 node = node as Node
 c = c as Controller
+def mindMapFile = node.mindMap.file
 
 def settings = [
-        customSaveDir      : node.mindMap.file.parent,
-//        customSaveDir      : "${node.mindMap.file.parent}/Mind-map_screenshots",
+        customSaveDir      : mindMapFile.parent,
         imageFileNamePrefix: node.mindMap.name,
+//        customSaveDir      : "${mindMapFile.parent}/images",
 //        imageFileNamePrefix: '',
 ]
+
+def customSaveDir = settings.customSaveDir ?: mindMapFile.parent
+if (!new File(customSaveDir).canWrite()) {
+    c.statusInfo = "(!) cannot write to ${customSaveDir}"
+    return
+}
 
 def transferable = Toolkit.defaultToolkit.systemClipboard.getContents(null)
 if (transferable.isDataFlavorSupported(DataFlavor.imageFlavor)) {
     def bufferedImage = transferable.getTransferData(DataFlavor.imageFlavor) as BufferedImage
-    def customSaveDir = settings.customSaveDir ?: node.mindMap.file.parent
     def imageFileNamePrefix = settings.imageFileNamePrefix ? "${settings.imageFileNamePrefix}_" : ''
     def imageFileName = "${imageFileNamePrefix}${node.id}_${new Date().format('yyyyMMdd_HHmmssS')}.png"
     def file = new File(customSaveDir, imageFileName)
@@ -35,7 +42,37 @@ if (transferable.isDataFlavorSupported(DataFlavor.imageFlavor)) {
     ImageIO.useCache = useCache
     def child = node.createChild()
     child.text = imageFileName
-    child.externalObject.file = file
+    if (config.getProperty('links') == 'relative') {
+        def filePathRelativeToMindMapParent = mindMapFile.parentFile.relativePath(file)
+        def uri = makeUriRetainingRelative(filePathRelativeToMindMapParent)
+        child.externalObject.uri = uri
+//        child.details = "URI: $uri"
+    } else {
+        child.externalObject.file = file
+//        child.details = "File: $file"
+    }
 } else {
     c.statusInfo = '(!) no image found in clipboard'
+}
+
+/**
+ * File#toURI() converts fsPaths to absolute paths
+ * This method works around it to allow relative fsPaths
+ * @param fsPath relative or absolute
+ * @return URI
+ */
+static URI makeUriRetainingRelative(String fsPath) {
+    return makeUriRetainingRelative(new File(fsPath))
+}
+
+static URI makeUriRetainingRelative(File file) {
+    if (file.absolute) {
+        return file.toURI()
+    } else {
+        def prefixPath = file.absolutePath[0..<-file.path.size()]
+        def prefixUriStr = new File(prefixPath).toURI().toString()
+        def absoluteUriStr = file.toURI().toString()
+        def relativeUriStr = absoluteUriStr[prefixUriStr.size()..-1]
+        return relativeUriStr.toURI()
+    }
 }
