0d88b8fa5aa34f458dc72afa81c0fb5363433c16
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-05-03 19:26:21 +0200

    new

---
+++ src/scripts/importFromHtmlTableClpb.groovy
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2025  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Import"})
+import io.github.macmarrum.freeplane.Import
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+
+c = c as Controller
+c.selecteds.each { Node n -> Import.fromClipboardHtmlTable(n) }
