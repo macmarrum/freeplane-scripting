e914cfa025dec79a0150a60a1e479883ce6b370b
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-05-04 23:41:28 +0200

    sync to Export changes

--- src/scripts/exportToJsonClpbSkip1.groovy
+++ src/scripts/exportToJsonClpbSkip1.groovy
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2024  macmarrum (at) outlook (dot) ie
+ * Copyright (C) 2024 - 2025  macmarrum (at) outlook (dot) ie
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Export"})
@@ -7,24 +7,26 @@ import io.github.macmarrum.freeplane.Export
 import org.freeplane.api.Node
 import org.freeplane.core.util.TextUtils
 
-def node = node as Node
+node = node as Node
 def settings = [
-//        details    : true,
-//        note       : true,
-//        attributes : true,
-//        transformed: true,
-//        dateFmt    : Export.DateFmt.ISO_LOCAL,
-//        format     : false,
-//        style      : true,
-//        formatting : true,
-//        icons      : true,
-//        tags       : true,
-//        link       : true,
-        skip1      : true,
-        denullify  : true,
-        pretty     : true,
-//        forceId    : false,
-//        forceAttribList: false,
+//        core           : false, // default: true
+//        details        : false, // default: true
+//        note           : false, // default: true
+//        attributes     : false, // default: true
+        transformed    : false, // default: true
+        plain          : false, // default: true
+        format         : true, // default: false
+        style          : true, // default: false
+//        formatting     : true, // default: false
+        icons          : true, // default: false
+        tags           : true, // default: false
+        link           : true, // default: false
+        skip1          : true, // default: false
+        denullify      : true, // default: false
+        pretty         : true, // default: false
+//        forceId        : true, // default: false
+//        forceAttribList: true, // default: false
+//        dateFmt        : Export.DateFmt.ISO_LOCAL,
 ]
 def text = Export.toJsonString(node, settings)
 TextUtils.copyToClipboard(text)
