b1f6cea9f7e9105381e1baaabd2dda3796894f32
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-06-13 17:10:47 +0200

    add table header; use newlineReplacement and quote settings

--- src/scripts/exportToMdTbodyClpbSkip1.groovy
+++ src/scripts/exportToMdTbodyClpbSkip1.groovy
@@ -1,9 +1,12 @@
-// Copyright (C) 2023, 2024  macmarrum (at) outlook (dot) ie
+// Copyright (C) 2023, 2024, 2025  macmarrum (at) outlook (dot) ie
 // SPDX-License-Identifier: GPL-3.0-or-later
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Export"})
 import io.github.macmarrum.freeplane.Export
 import org.freeplane.core.util.TextUtils
 
-def settings = [sep: '|', tail: true, skip1: true]
+def settings = [skip1: true, sep: '|', tail: true, nl: '\u00B6', quote: 'NONE']
 def text = Export.toCsvString(node, settings)
-TextUtils.copyToClipboard(text)
+def line = text.split(/\n/, 2)[0]
+def numOfSeps = line.count('|')
+def mdTable = "${'|' * numOfSeps}\n${'|-' * (numOfSeps - 1)}|\n${text}"
+TextUtils.copyToClipboard(mdTable)
