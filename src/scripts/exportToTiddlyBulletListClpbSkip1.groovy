d9c02f1e2c82009545e72abe87d4a27c4aff3186
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-06-25 10:12:13 +0200

    new

---
+++ src/scripts/exportToTiddlyBulletListClpbSkip1.groovy
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2025  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Export"})
+
+
+import io.github.macmarrum.freeplane.Export
+import org.freeplane.api.Node
+import org.freeplane.core.util.TextUtils
+
+node = node as Node
+def settings = [
+        mkBullet   : { int level, Node n -> ('*' * level) + ' ' },
+//        transformed: true,
+//        plain      : true,
+//        nl         : '\u006B', // pilcrow
+//        link       : true,
+        skip1      : true,
+]
+def bulletList = Export.toBulletListString(node, settings)
+TextUtils.copyToClipboard(bulletList)
