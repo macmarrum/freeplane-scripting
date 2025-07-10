b88cfc661ab7febf188815ed0e04ec0582a58126
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-07-10 21:40:57 +0200

    add duplicateSelectedBranches.groovy

---
+++ src/scripts/duplicateSelectedBranches.groovy
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2025  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Duplicate"})
+
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+
+c = c as Controller
+// differentSubtrees - if true children/grandchildren/grandgrandchildren/... nodes of selected parent nodes are excluded from the result.
+def differentSubtrees = true
+c.getSortedSelection(differentSubtrees).each { Node n ->
+    def parent = n.parent
+    parent.appendBranch(n)
+    def newNode = parent.children[-1]
+    def nPosition = parent.getChildPosition(n)
+    newNode.moveTo(parent, nPosition + 1)
+}
