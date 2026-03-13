10cfdf7ec5f4bed2f51fc58a3c02c5edcc1d008a
macmarrum <7197131+macmarrum@users.noreply.github.com>
2026-03-13 09:57:01 +0100

    use sprintf for pattern and replacement; refactor

--- src/scripts/replaceRegex.groovy
+++ src/scripts/replaceRegex.groovy
@@ -1,18 +1,28 @@
+/*
+ * Copyright (C) 2022, 2024, 2026  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 // @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1"})
-import org.freeplane.api.Node as FPN
+
+
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
 import org.freeplane.core.resources.ResourceController
 import org.freeplane.core.ui.components.UITools
 import org.freeplane.features.map.NodeModel
-import org.freeplane.plugin.script.proxy.ScriptUtils
 
-final String default_browser_command_windows_9x = "default_browser_command_windows_9x"
+c = c as Controller
+final String _last_replace_regex_expression = "_last_replace_regex_expression"
 final resourceController = ResourceController.getResourceController()
-final c = ScriptUtils.c()
 final NodeModel nodeModel = c.selected.delegate
-final String initialValue = resourceController.getProperty(default_browser_command_windows_9x)
-String input = UITools.showInputDialog(nodeModel, 'Replace Regex\nE.g. /pattern/replacement/g\nNB Special characters <([{\\^-=$!|]})?*+.>', initialValue)
+final String initialValue = resourceController.getProperty(_last_replace_regex_expression)
+// printf-style → https://docs.oracle.com/javase/8/docs/api/java/util/Formatter.html#syntax
+String input = UITools.showInputDialog(nodeModel, '''Replace Regex
+E.g. /pattern/replacement/g
+Use printf-style, e.g. %n, for pattern and replacement
+NB Special characters <([{\\^-=$!|]})?*+.>''', initialValue)
 if (input !== null) {
-    resourceController.setProperty(default_browser_command_windows_9x, input)
+    resourceController.setProperty(_last_replace_regex_expression, input)
     final token = input[0]
     final tokenCount = input.count(token)
     if (tokenCount != 3) {
@@ -20,14 +30,14 @@ if (input !== null) {
         return
     }
     final lst = input.tokenize(token)
-    final String pattern = lst[0]
+    final String pattern = sprintf(lst[0])
     final boolean isGlobal = input.endsWith("${token}g")
     String replacement
     if ((isGlobal && input.endsWith("$token${token}g")) || input.endsWith("$token$token"))
         replacement = ''
     else
-        replacement = lst[1]
-    c.selecteds.each { FPN n ->
+        replacement = sprintf(lst[1])
+    c.selecteds.each { Node n ->
         if (isGlobal)
             n.text = n.text.replaceAll(pattern, replacement)
         else
