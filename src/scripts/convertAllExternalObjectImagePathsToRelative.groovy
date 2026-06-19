75acc3dadddfe211a0b77299bbd7ab1ff23d8b9f
macmarrum <7197131+macmarrum@users.noreply.github.com>
2026-06-19 21:16:28 +0200

    add convertAllExternalObjectImagePathsToRelative.groovy

---
+++ src/scripts/convertAllExternalObjectImagePathsToRelative.groovy
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2026  macmarrum (at) outlook (dot) ie
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/Convert"})
+import org.freeplane.api.Controller
+import org.freeplane.api.Node
+import org.freeplane.features.link.LinkController
+
+c = c as Controller
+node = node as Node
+
+def mindMapFile = node.mindMap.file
+
+c.findAll().each { Node n ->
+    String uriStr = n.externalObject?.uri
+    if (uriStr) {
+        URI uri = uriStr.toURI()
+        def file = new File(uri)
+        if (file.isAbsolute()) {
+            n.externalObject.uri = LinkController.toLinkTypeDependantURI(mindMapFile, file, LinkController.LINK_RELATIVE_TO_MINDMAP)
+        }
+    }
+}
