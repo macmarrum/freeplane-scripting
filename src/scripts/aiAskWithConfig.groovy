4259ad32a25fddc48a2d554b6551e781179c12a0
macmarrum <7197131+macmarrum@users.noreply.github.com>
2026-06-21 14:16:55 +0200

    rename; use aiRequestOptions as of 1.13.3-pre11: systemMessage, SHOW_IN_NEW_CHAT (renamed); use provider|model set in UI

---
+++ src/scripts/aiAskWithConfig.groovy
@@ -0,0 +1,37 @@
+// Copyright (C) 2026  macmarrum (at) outlook (dot) ie
+// SPDX-License-Identifier: GPL-3.0-or-later
+// @ExecutionModes({ON_SINGLE_NODE="/menu_bar/Mac1/AI"})
+
+
+import org.freeplane.api.Controller
+import org.freeplane.api.ai.*
+
+import java.time.Duration
+
+c = c as Controller
+def node = c.selected
+
+//def providerModel = ['ollama', 'phi4-mini:latest']
+//def providerModel = ['OpenRouter', 'nex-agi/nex-n2-pro:free']
+def providerModel = config.getProperty('ai_selected_model').split('\\|', 2)
+
+def systemMessage = node.parent.text ?: null  // fall back to default if no parent text
+def prompt = node.text
+
+def aiRequestOptions = AiRequestOptions.builder()
+        .systemMessage(systemMessage)
+        .modelSelection(AiModelSelection.explicit(*providerModel))
+        .toolAvailability(AiToolAvailability.DISABLED)
+        .mode(AiRequestMode.SHOW_IN_NEW_CHAT)
+        .timeout(Duration.ofSeconds(60))
+        .build()
+//println("${new Date().format('HH:mm:ss')} askAi: ${prompt} | ${aiRequestOptions.mode.name()}")
+c.askAi(prompt, aiRequestOptions) { result ->
+    if (result.status == AiRequestStatus.SUCCEEDED) {
+        node.text = result.response
+        node.details = "askAi prompt: ${prompt}"
+    } else {
+        node.details = "askAi status: ${result.status.name()}"
+    }
+    node.note = result.detail
+}
